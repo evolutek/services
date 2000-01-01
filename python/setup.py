00eeda9ab07a6a3990143f04b4e6e2c82269577e
Corentin Vigourt <corentin.vigourt@epita.fr>
2000-01-01 01:15:25 +0100

    [SETUP] added develop class for copying library

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from subprocess import check_call
+
+class PostDevelopCommand(develop):
+    def run(self):
+        check_call("cp -r evolutek/ /usr/lib/python3.4/site-packages/".split())
+        develop.run(self)
 
 setup(
     name = "services",
     version = "2018",
     packages = find_packages(),
     namespace_packages = ['evolutek'],
+
+    cmdclass = {
+        'develop': PostDevelopCommand
+    },
 
     install_requires = open('requirements.pip').read().splitlines(),
     extra_requires = {
