3e5179ce004176282c47bc209a2260e20dcf0e0c
Rémi (halfr) Audebert <mail@halfr.net>
2012-12-01 18:03:16 +0100

    ax-python: better stress test

--- ax-python/stress.py
+++ ax-python/stress.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
 from random import randint
 from time import sleep
 from cellaserv.proxy import CellaservProxy
 
 def main():
+    if len(sys.argv) < 2:
+        print("Error: expect ax ids as argument", file=sys.stderr)
+        sys.exit(1)
+
     cs = CellaservProxy()
+    axs = [cs.ax[int(ax_id)] for ax_id in sys.argv[1:]]
     while True:
-        cs.ax("3").move(goal=randint(200, 800))
-        sleep(0.5)
-        cs.ax("5").move(goal=randint(200, 800))
-        sleep(0.5)
+        for ax in axs:
+            ax.move(goal=randint(200, 800))
+            sleep(0.5)
 
 if __name__ == '__main__':
     main()
