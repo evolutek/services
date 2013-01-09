d1011d304322023e37b6428894b2942e75871ca6
Rémi (halfr) Audebert <mail@halfr.net>
2013-01-10 00:55:24 +0100

    Proper use of double-colon rules in Makefile

--- install.mk
+++ install.mk
@@ -4,7 +4,7 @@ INSTALL_FILE   = install -m 644 -p -D
 INSTALL_EXEC   = install -m 755 -p -D
 INSTALL_LIB    = $(INSTALL_EXEC)
 
-install:: install_service_files install_systemd_files install_custom
+install:: install_service_files install_systemd_files
 
 install_service_files:: $(EXE)
 	$(INSTALL_EXEC) $(EXE) $(PREFIX)/usr/lib/cs_services/$(SERVICE)
@@ -15,16 +15,12 @@ install_systemd_files:: $(SYSTEMD_FILES)
 	    sed -i 's,$$PREFIX,$(PREFIX),g' $(SYSTEMD_PREFIX)/$$(basename $$file); \
 	done
 
-install_custom::
+uninstall:: uninstall_service_files uninstall_systemd_files
 
-uninstall: uninstall_service_files uninstall_systemd_files uninstall_custom
-
-uninstall_service_files:
+uninstall_service_files::
 	rm -f $(PREFIX)/usr/lib/cs_services/$(SERVICE)
 
-uninstall_systemd_files:
+uninstall_systemd_files::
 	for file in $(SYSTEMD_FILES); do \
 	    rm -f $(SYSTEMD_PREFIX)/$$file; \
 	done
-
-uninstall_custom::
