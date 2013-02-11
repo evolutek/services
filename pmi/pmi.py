f3c68c88e9baa82b84231855a203b9d2939f2d59
Rémi (halfr) Audebert <mail@halfr.net>
2013-02-11 11:51:40 +0100

    pmi: Update interface

--- pmi/pmi.py
+++ pmi/pmi.py
@@ -7,8 +7,10 @@ from cellaserv.service import Service
 AX_ID_PINCE_GAUCHE = "1"
 AX_ID_PINCE_DROITE = "6"
 AX_ID_ASCENSSEUR = "2"
+AX_ID_ROUE_GAUCHE = "5"
+AX_ID_ROUE_DROITE = "3"
 
-DELTA_PINCES = 40
+DELAY = 0.1
 
 class PMI(Service):
 
@@ -17,45 +19,53 @@ class PMI(Service):
 
         self.cs = CellaservProxy(self)
 
+    @Serivce.action
+    def reset(self):
+        self.cs.ax[AX_ID_ASCENSSEUR].mode_joint()
+        self.cs.ax[AX_ID_ROUE_GAUCHE].mode_wheel()
+        self.ouvrir_pinces()
+        self.ascensseur_bas()
+
     @Service.action
     def ouvrir_pinces(self):
         self.cs.ax[AX_ID_PINCE_GAUCHE].mode_joint()
-        sleep(0.1)
+        sleep(DELAY)
         self.cs.ax[AX_ID_PINCE_DROITE].mode_joint()
-        sleep(0.1)
+        sleep(0.3)
 
-        self.cs.ax[AX_ID_PINCE_GAUCHE].move(goal=460)
-        sleep(0.1)
-        self.cs.ax[AX_ID_PINCE_DROITE].move(goal=940)
-        sleep(0.1)
+        self.cs.ax[AX_ID_PINCE_GAUCHE].move(goal=450)
+        sleep(DELAY)
+        self.cs.ax[AX_ID_PINCE_DROITE].move(goal=550)
+        sleep(DELAY)
 
     @Service.action
     def fermer_pinces(self):
         self.cs.ax[AX_ID_PINCE_GAUCHE].mode_wheel()
-        sleep(0.1)
+        sleep(DELAY)
         self.cs.ax[AX_ID_PINCE_DROITE].mode_wheel()
-        sleep(0.1)
+        sleep(DELAY)
 
-        self.cs.ax[AX_ID_PINCE_GAUCHE].moving_speed(speed=300)
-        sleep(0.1)
-        self.cs.ax[AX_ID_PINCE_DROITE].moving_speed(speed=2**10 | 300)
+        self.cs.ax[AX_ID_PINCE_GAUCHE].turn(side=False, speed=300)
+        sleep(DELAY)
+        self.cs.ax[AX_ID_PINCE_DROITE].turn(side=True, speed=300)
 
     @Service.action
     def ascensseur_bas(self):
-        self.cs.ax[AX_ID_ASCENSSEUR].mode_wheel()
-        sleep(0.1)
-        self.cs.ax[AX_ID_ASCENSSEUR].moving_speed(speed=2**10 | 1000)
-        sleep(1)
-        self.cs.ax[AX_ID_ASCENSSEUR].moving_speed(speed=0)
+        self.cs.ax[AX_ID_ASCENSSEUR].mode_joint()
+        sleep(DELAY)
+        self.cs.ax[AX_ID_ASCENSSEUR].move(goal=0)
 
     @Service.action
     def ascensseur_haut(self):
-        self.cs.ax[AX_ID_ASCENSSEUR].mode_wheel()
-        sleep(0.1)
-        self.cs.ax[AX_ID_ASCENSSEUR].moving_speed(speed=1000)
-        sleep(1)
-        self.cs.ax[AX_ID_ASCENSSEUR].moving_speed(speed=0)
+        self.cs.ax[AX_ID_ASCENSSEUR].mode_joint()
+        sleep(DELAY)
+        self.cs.ax[AX_ID_ASCENSSEUR].move(goal=1023)
 
+    @Service.action
+    def avancer(self):
+        self.cs.ax[AX_ID_ROUE_GAUCHE].turn(side=False, speed=1023)
+        sleep(DELAY)
+        self.cs.ax[AX_ID_ROUE_DROITE].turn(side=True, speed=1023)
 
 def main():
     pmi = PMI()
