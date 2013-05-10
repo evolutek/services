888d27a5ddaecb29efdbcb9b8801a9167d0d88ca
Rémi (halfr) Audebert <mail@halfr.net>
2013-05-10 12:08:04 +0200

    trajman: set wheels

--- trajman/trajman.py
+++ trajman/trajman.py
@@ -78,6 +78,11 @@ class TrajMan(Service):
 
         self.soft_free_state = False
 
+        self.flush_serial()
+        #self.set_wheels_diameter(w1=53.234, w2=54.248)
+        self.set_wheels_diameter(w1=54, w2=54)
+        self.set_wheels_spacing(spacing=302.447)
+
     def log_debug(self, *args, **kwargs):
         if PRINT_DEBUG:
             print(*args, **kwargs)
