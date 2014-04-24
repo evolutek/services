3eb127f5f841eb1c92838a740a436f30e2a7d28c
Rémi (halfr) Audebert <halfr@half.net>
2014-04-24 19:09:24 +0200

    config: add test

---
+++ config/tests/test_config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from cellaserv.service import Service, ConfigVariable
+
+class TestConfig(Service):
+    foo = ConfigVariable("test", "foo")
+
+    @Service.action
+    def test(self):
+        return foo
+
+def main():
+    service = TestConfig()
+    service.run()
+
+if __name__ == '__main__':
+    main()
