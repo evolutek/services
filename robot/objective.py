b0bd11a6de8da2d21a1453b5fcffcd5cfcc23d04
Benoît Reitz <nebryum@free.fr>
2014-05-26 03:53:36 +0200

    New set of objectives

--- robot/objective.py
+++ robot/objective.py
@@ -3,7 +3,7 @@
 from abc import ABCMeta, abstractmethod
 from time import sleep
 
-import math
+from math import pi, cos, sin
 
 class Objective(metaclass=ABCMeta):
 
@@ -53,20 +53,36 @@ class FirePlaceDrop(Objective):
     def execute(self, robot, cs, status):
         robot.goto_theta_block(self.direction)
         sleep(.5)
-        robot.goto_xy_block(self.x + 130 * math.cos(self.direction), self.y +
-                130 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 130 * cos(self.direction), self.y +
+                130 * sin(self.direction))
         cs.actuators.collector_open()
         sleep(.5)
         cs.actuators.collector_fireplace()
-        robot.goto_xy_block(self.x + 50 * math.cos(self.direction), self.y +
-                50 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 50 * cos(self.direction), self.y +
+                50 * sin(self.direction))
         cs.actuators.collector_close()
         sleep(1)
-        robot.goto_xy_block(self.x + 130 * math.cos(self.direction), self.y +
-                130 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 130 * cos(self.direction), self.y +
+                130 * sin(self.direction))
         robot.goto_xy_block(self.x, self.y)
         status.has_fire = False
 
+class FirePlaceDropCenter(FirePlaceDrop):
+    def execute(self, robot, cs, status):
+        robot.goto_theta_block(self.direction)
+        robot.goto_xy_block(self.x + 100 * cos(self.direction), self.y +
+                100 * sin(self.direction))
+        cs.actuators.collector_open()
+        sleep(.5)
+        cs.actuators.collector_fireplace()
+        robot.goto_xy_block(self.x + 50 * cos(self.direction), self.y +
+                50 * sin(self.direction))
+        cs.actuators.collector_close()
+        sleep(1)
+        robot.goto_xy_block(self.x + 100 * cos(self.direction), self.y +
+                100 * sin(self.direction))
+        robot.goto_xy_block(self.x, self.y)
+        status.has_fire = False
 
 class StandingFire(Objective):
 
@@ -80,14 +96,14 @@ class StandingFire(Objective):
 
     def execute(self, robot, cs, status):
         robot.goto_theta_block(self.direction)
-        robot.goto_xy_block(self.x + 300 * math.cos(self.direction),
-                            self.y + 300 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 300 * cos(self.direction),
+                            self.y + 300 * sin(self.direction))
         cs.actuators.collector_open()
-        robot.goto_xy_block(self.x + 200 * math.cos(self.direction),
-                            self.y + 200 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 200 * cos(self.direction),
+                            self.y + 200 * sin(self.direction))
         cs.actuators.collector_down()
-        robot.goto_xy_block(self.x + 350 * math.cos(self.direction),
-                            self.y + 350 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 350 * cos(self.direction),
+                            self.y + 350 * sin(self.direction))
         cs.actuators.collector_close()
         sleep(.5)
         cs.actuators.collector_hold()
@@ -96,12 +112,12 @@ class StandingFire(Objective):
         sleep(1)
         if not cs.actuators.collector_has_fire():
             cs.actuators.collector_open()
-            robot.goto_xy_block(self.x + 250 * math.cos(self.direction),
-                                self.y + 250 * math.sin(self.direction))
+            robot.goto_xy_block(self.x + 250 * cos(self.direction),
+                                self.y + 250 * sin(self.direction))
             cs.actuators.collector_down()
             sleep(.5)
-            robot.goto_xy_block(self.x + 450 * math.cos(self.direction),
-                                self.y + 450 * math.sin(self.direction))
+            robot.goto_xy_block(self.x + 450 * cos(self.direction),
+                                self.y + 450 * sin(self.direction))
             cs.actuators.collector_close()
             sleep(.5)
             cs.actuators.collector_hold()
@@ -115,33 +131,33 @@ class StandingFire(Objective):
 
     def __str__(self):
         return "Standing fire " +\
-                str(self.x + 200 * math.cos(self.direction)) +\
-                " " + str(self.y + 200 * math.sin(self.direction))
+                str(self.x + 200 * cos(self.direction)) +\
+                " " + str(self.y + 200 * sin(self.direction))
 
 class WallFire(Objective):
 
     def execute(self, robot, cs, status):
         robot.goto_theta_block(self.direction)
-        robot.goto_xy_block(self.x + 100 * math.cos(self.direction),
-                            self.y + 100 * math.sin(self.direction))
+        robot.goto_xy_block(self.x + 100 * cos(self.direction),
+                            self.y + 100 * sin(self.direction))
         robot.goto_xy_block(self.x, self.y)
-        robot.goto_theta_block(2 * math.pi - self.direction)
+        robot.goto_theta_block(2 * pi - self.direction)
 
     def __str__(self):
         return "Wall fire " +\
-                str( self.x + 100 * math.cos(self.direction)) +\
-                " " + str(self.y + 100 * math.sin(self.direction))
+                str( self.x + 100 * cos(self.direction)) +\
+                " " + str(self.y + 100 * sin(self.direction))
 
 class Torch(Objective):
 
     def execute(self, robot, cs, status):
         robot.goto_theta_block(0)
-        robot.goto_theta_block(math.pi / 2)
+        robot.goto_theta_block(pi / 2)
 
     def __str__(self):
         return "Torche fire " +\
-               str( self.x + 200 * math.cos(self.direction)) +\
-                " " + str(self.y + 200 * math.sin(self.direction))
+               str( self.x + 200 * cos(self.direction)) +\
+                " " + str(self.y + 200 * sin(self.direction))
 
 class DefaultObjectives():
 
@@ -155,27 +171,45 @@ class DefaultObjectives():
         one depending on the robot's color"""
 
         defobj = ObjectiveList()
-        positions = [[1650, 350, -math.pi / 4]]
+
+        # Objective needed to exit the startup zone
+        defobj.append(FedexObjective(1, *DefaultObjectives.color_pos(color,
+            437, 232, 0)))
+
+        # Both corner fireplace
+        positions = [
+                [1650, 350, -pi / 4],
+                [1650, 2650, pi / 4],
+        ]
         for pos in positions:
             defobj.append(FirePlaceDrop(1, *DefaultObjectives.color_pos(color,
                 pos[0], pos[1], pos[2])))
 
-        defobj.append(FedexObjective(1, *DefaultObjectives.color_pos(color,
-            437, 232, 0)))
+        # Standing fire. all 6 of them are declared because of the change
+        # needed depending on which color you are
         positions = [
                 [800, 400, 0],
-        #        [1400, 2600, math.pi],
-        #        [600, 600, math.pi / 2],
-        #        [600, 1900, math.pi / 2],
-        #        [1600, 1200, -math.pi / 2],
-        #        [1600, 2400, -math.pi / 2],
+                [1300, 2600, pi],
+                [600, 700, pi / 2],
+                [600, 1900, pi / 2],
+                [1600, 1100, -pi / 2],
+                [1600, 2300, -pi / 2],
         ]
         for pos in positions:
             defobj.append(StandingFire(1, *DefaultObjectives.color_pos(color,
                 pos[0], pos[1], pos[2])))
+
+
+        # Center fire. Is defined as multiple position to prevent stacking
+        # fires
+        for i in range(8):
+            defobj.append(FirePlaceDropCenter(2, 1050 + cos(pi/4 * i) * 400, 1500 +
+                sin(pi/4 * i) * 400, pi - pi/8*i))
+
         return defobj
+
         positions = [
-                [800, 400, -math.pi / 2],
+                [800, 400, -pi / 2],
                 [1600, 1300, 0],
                 ]
         for pos in positions:
