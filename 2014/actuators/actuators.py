fb6bb551c9572728278540aca706f5be381c5836
Samuel "Staphylo" Angebault <staphyloa@gmail.com>
2014-05-29 05:24:37 +0200

    actuators: adding pmi actuator motions

--- 2014/actuators/actuators.py
+++ 2014/actuators/actuators.py
@@ -4,6 +4,7 @@ from time import sleep
 from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Service
 
+# main axs
 AX_ID_COLLECT_ROTATION = "10"
 AX_ID_COLLECT = "11"
 AX_ID_COLLECT_ELEVATOR = "12"
@@ -17,6 +18,13 @@ AX_COLLECTOR_CLOSE = 650
 AX_COLLECTOR_PUSH_FIRE = 350
 AX_ROTATION_START = 500
 AX_ROTATION_END = 138
+
+# pmi axs
+AX_ID_FRUITS = "3"
+
+AX_FRUITS_CLOSE = 500
+AX_FRUITS_GET = 850
+AX_FRUITS_FLUSH = 960
 
 # others to come
 
@@ -34,13 +42,20 @@ class Actuators(Service):
         self.cs = CellaservProxy()
         self.rotation = AX_ROTATION_START
 
+    def setup(self):
+        super().setup()
+        self.cs.ax[AX_ID_FRUITS].mode_joint()
+        self.cs.ax[AX_COLLECT].mode_joint()
+        self.cs.ax[AX_ID_COLLECT_ELEVATOR].mode_joint()
+        self.cs.ax[AX_ID_COLLECT_ROTATION].mode_joint()
 
     @Service.action
     def free(self):
         for ax in [
                 AX_ID_COLLECT,
                 AX_ID_COLLECT_ELEVATOR,
-                AX_ID_COLLECT_ROTATION
+                AX_ID_COLLECT_ROTATION,
+                AX_ID_FRUITS
                 ]:
             self.cs.ax[ax].free()
 
@@ -117,6 +132,22 @@ class Actuators(Service):
     @Service.action
     def collector_has_fire(self):
         return 570 < self.cs.ax[AX_ID_COLLECT].get_present_position()
+
+    @Service.action
+    def fruits_get(self):
+        self.cs.ax[AX_ID_FRUITS].move(goal=AX_FRUITS_GET)
+
+    @Service.action
+    def fruits_close(self):
+        self.cs.ax[AX_ID_FRUITS].move(goal=AX_FRUITS_CLOSE)
+
+    @Service.action
+    def fruits_flush(self):
+        for i in range(0, 8):
+            self.cs.ax[AX_ID_FRUITS].move(goal=AX_FRUITS_FLUSH)
+            sleep(.3)
+            self.cs.ax[AX_ID_FRUITS].move(goal=AX_FRUITS_GET)
+            sleep(.3)
 
     @Service.action
     def test(self):
