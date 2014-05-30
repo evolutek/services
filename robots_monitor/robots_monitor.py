60f2afe143a250589fa873583c831994c5f6ba49
Rémi Audebert <halfr@halfr.net>
2014-05-30 05:31:56 +0200

    robots_monitor: big refactor, more reliable

--- robots_monitor/robots_monitor.py
+++ robots_monitor/robots_monitor.py
@@ -4,58 +4,54 @@ from time import sleep
 from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Service, ConfigVariable
 
-@Service.require('trajman')
+
 class RobotsMonitor(Service):
 
     freq = ConfigVariable(section="monitor", option="frequency", coerc=float)
-    mon_pos_pal = ConfigVariable(section="monitor", option="position_pal",
-            coerc=eval)
-    mon_pos_pmi = ConfigVariable(section="monitor", option="position_pmi",
-            coerc=eval)
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, identification=None):
+        super().__init__(identification=identification)
 
         self.cs = CellaservProxy()
-
-        self.monitored = set()
-        self.mon_pos_pal.add_update_cb(lambda value:
-                self.monitored.add(self.position_pal) if value else
-                self.monitored.remove(self.position_pal))
-        self.mon_pos_pmi.add_update_cb(lambda value:
-                self.monitored.add(self.position_pmi) if value else
-                self.monitored.remove(self.position_pmi))
-
-        self.pal = self.cs.trajman['pal']
-        self.pmi = self.cs.trajman['pmi']
+        self.tm = self.cs.trajman[self.robot_name]
 
     def setup(self):
         super().setup()
 
-        if self.mon_pos_pal():
-            self.monitored.add(self.position_pal)
-        if self.mon_pos_pmi():
-            self.monitored.add(self.position_pmi)
-
-    def position_pal(self):
+    def position(self):
         log_name = 'log.monitor.robot_position'
-        pos = self.pal.get_position()
+        pos = self.tm.get_position()
         if pos is not None:
-            self(log_name, robot="pal", **pos)
-
-    def position_pmi(self):
-        log_name = 'log.monitor.robot_position'
-        self(log_name, robot="pmi", **self.pmi.get_position())
+            self(log_name, robot=self.robot_name, **pos)
 
     @Service.thread
     def loop(self):
-        while not sleep(float(self.freq())):
-            for mon_func in self.monitored:
-                mon_func()
+        while not sleep(self.freq()):
+            self.position()
+
+
+@Service.require('trajman.pal')
+class MonitorPal(RobotsMonitor):
+
+    def __init__(self):
+        self.robot_name = 'pal'
+        super().__init__('pal')
+
+
+@Service.require('trajman.pmi')
+class MonitorPmi(RobotsMonitor):
+
+    def __init__(self):
+        self.robot_name = 'pmi'
+        super().__init__('pmi')
+
 
 def main():
-    robots_monitor = RobotsMonitor()
-    robots_monitor.run()
+    pal = MonitorPal()
+    pal.setup()
+    pmi = MonitorPmi()
+    pmi.setup()
+    Service.loop()
 
 if __name__ == '__main__':
     main()
