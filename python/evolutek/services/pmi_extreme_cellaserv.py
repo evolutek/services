609cb139e24b3b8abf598397cb4632a8f1d1de0c
Corentin Vigourt <corentin.vigourt@epita.fr>
2015-05-08 23:22:32 +0200

    IA correction

--- python/evolutek/services/pmi_extreme_cellaserv.py
+++ python/evolutek/services/pmi_extreme_cellaserv.py
@@ -1,71 +1,54 @@
-import mraa
 from time import sleep
-import thread
 from cellaserv.service import Service, Event, Variable, ConfigVariable
 from cellaserv.proxy import CellaservProxy
 
-# http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
-
 
 class IaPMI(Service):
-    # match_start = Variable('start')
-    color = ConfigVariable(section='match', option='color',  coerc=lambda v: {'green': -1, 'yellow': 1}[v])
-    speed = ConfigVariable(section='pmi', option='trsl_max', coerc=float)
-
-    # Code for Ax 12
-    # 1 = droite_avant
-    # 2 = gauche_arriere
-    # 3 = droite_arriere
-    # 4 = gauche_avant
+    speed = 700
+    color = -1
+    timer = 0
 
     def __init__(self):
-        timer = 0
-        # thread.start_new_thread(dodge,("ThreadScharp",10,11))
-        # thread.start_new_thread(tirette,("ThreadTirette",15))
-        # init ax12 lib
         cs = CellaservProxy()
         cs.ax["1"].mode_wheel()
         cs.ax["2"].mode_wheel()
         cs.ax["3"].mode_wheel()
         cs.ax["4"].mode_wheel()
         cs.ax["5"].mode_joint()
-        # test if the cord is connected
-        # while(need_to_dodge):
-        # time.sleep(10)
         print("PMI : Wait")
         match_start = Event('start')
         match_start.wait()
         print("PMI : start")
-        move_to_stairs()
+        self.move_to_stairs()
         print("PMI : finish")
 
-    def marche_avant(x):
-        cs.ax["1"].turn(True, speed)
-        cs.ax["2"].turn(False, speed)
-        cs.ax["3"].turn(True, speed)
-        cs.ax["4"].turn(False, speed)
+    def marche_avant(self, x, cs):
+        cs.ax["1"].turn(True, self.speed)
+        cs.ax["2"].turn(False, self.speed)
+        cs.ax["3"].turn(True, self.speed)
+        cs.ax["4"].turn(False, self.speed)
         sleep(1)
-        cs.ax["1"].turn(True, speed)
-        cs.ax["2"].turn(False, speed)
-        cs.ax["3"].turn(True, speed)
-        cs.ax["4"].turn(False, speed)
+        cs.ax["1"].turn(True, self.speed)
+        cs.ax["2"].turn(False, self.speed)
+        cs.ax["3"].turn(True, self.speed)
+        cs.ax["4"].turn(False, self.speed)
         sleep(x)
         print("Marche avant : done")
 
-    def marche_arriere(x):
-        cs.ax["1"].turn(False, speed)
-        cs.ax["2"].turn(True, speed)
-        cs.ax["3"].turn(False, speed)
-        cs.ax["4"].turn(True, speed)
+    def marche_arriere(self, x, cs):
+        cs.ax["1"].turn(False, self.speed)
+        cs.ax["2"].turn(True, self.speed)
+        cs.ax["3"].turn(False, self.speed)
+        cs.ax["4"].turn(True, self.speed)
         sleep(1)
-        cs.ax["1"].turn(False, speed)
-        cs.ax["2"].turn(True, speed)
-        cs.ax["3"].turn(False, speed)
-        cs.ax["4"].turn(True, speed)
+        cs.ax["1"].turn(False, self.speed)
+        cs.ax["2"].turn(True, self.speed)
+        cs.ax["3"].turn(False, self.speed)
+        cs.ax["4"].turn(True, self.speed)
         sleep(x)
         print("Marche arriere : done")
 
-    def arret(x):
+    def arret(self, x, cs):
         cs.ax["1"].turn(True, 0)
         cs.ax["2"].turn(True, 0)
         cs.ax["3"].turn(True, 0)
@@ -78,93 +61,71 @@ class IaPMI(Service):
         sleep(x)
         print("Arret : done")
 
-    def rotation_gauche(x):
-        cs.ax["1"].turn(True, speed)
-        cs.ax["2"].turn(True, speed)
-        cs.ax["3"].turn(True, speed)
-        cs.ax["4"].turn(True, speed)
+    def rotation_gauche(self, x, cs):
+        cs.ax["1"].turn(True, self.speed)
+        cs.ax["2"].turn(True, self.speed)
+        cs.ax["3"].turn(True, self.speed)
+        cs.ax["4"].turn(True, self.speed)
         sleep(1)
-        cs.ax["1"].turn(True, speed)
-        cs.ax["2"].turn(True, speed)
-        cs.ax["3"].turn(True, speed)
-        cs.ax["4"].turn(True, speed)
+        cs.ax["1"].turn(True, self.speed)
+        cs.ax["2"].turn(True, self.speed)
+        cs.ax["3"].turn(True, self.speed)
+        cs.ax["4"].turn(True, self.speed)
         sleep(x)
         print("Rotation gauche : done")
 
-    def rotation_droite(x):
-        cs.ax["1"].turn(False, speed)
-        cs.ax["2"].turn(False, speed)
-        cs.ax["3"].turn(False, speed)
-        cs.ax["4"].turn(False, speed)
+    def rotation_droite(self, x, cs):
+        cs.ax["1"].turn(False, self.speed)
+        cs.ax["2"].turn(False, self.speed)
+        cs.ax["3"].turn(False, self.speed)
+        cs.ax["4"].turn(False, self.speed)
         sleep(1)
-        cs.ax["1"].turn(False, speed)
-        cs.ax["2"].turn(False, speed)
-        cs.ax["3"].turn(False, speed)
-        cs.ax["4"].turn(False, speed)
+        cs.ax["1"].turn(False, self.speed)
+        cs.ax["2"].turn(False, self.speed)
+        cs.ax["3"].turn(False, self.speed)
+        cs.ax["4"].turn(False, self.speed)
         sleep(x)
         print("Rotation droite : done")
 
-    def depose_tapis():
+    def depose_tapis(self, cs):
         cs.ax["5"].move(goal=0)
         sleep(500)
         cs.ax["5"].move(goal=1024)
         cs.ax["5"].move(goal=512)
         print("Depose tapis : done")
 
-    # this thread manage when you have to dodge
-   """ def dodge ( threadName,ping,pind, is_activated):
-        while(True):
-            if(!is_activated):
-                need_to_dodge = false
+    def move_to_stairs(self, cs):
+        while self.timer != 6000:
+            self.marche_avant(1)
+            self.timer = self.timer + 2
+
+        self.arret(1000)
+        self.timer = 0
+        while self.timer != 1300:
+            if self.color == -1:
+                self.rotation_gauche(1)
             else:
-                need_to_dodge = ((mraa.read(ping).read() < 100)or(mraa.read(pind).read() < 100))
-"""
-
-    
-
-    #this thread tell you wh...oh wait
-    #def tirette (threadName,pin):
-    #   while(True):
-    #       need_to_dodge = (mraa.read(pin).read() > 512)
-
-    #IA part
-    def move_to_stairs(self):
-        while timer != 6000:
-            #while need_to_dodge:
-                #arret(1); # if sharps those not work test acquisition time
-            marche_avant(1)
-            timer = timer + 2;
-
-        arret(1000)
-        timer = 0
-        while timer != 1300:
-            #while need_to_dodge:
-               # arret(1)
-            if color == -1:
-                rotation_gauche(1)
+                self.rotation_droite(1)
+            self.timer = self.timer+2
+        self.arret(1000)
+        self.timer = 0
+        while self.timer != 100:
+            self.marche_avant(1)
+            self.timer = self.timer + 2
+            self.marche_avant(1)
+            self.timer = self.timer + 2
+        self.timer = 0
+        while self.timer != 6500:
+            if self.timer == 1500:
+                self.arret(1)
+                self.depose_tapis()
             else:
-                rotation_droite(1)
-            timer = timer+2
-        arret(1000)
-        timer = 0
-        while timer != 100:
-            #while need_to_dodge:
-               # arret(1)
-            marche_avant(1)
-            timer = timer + 2
-            marche_avant(1)
-            timer = timer + 2
-        timer = 0
-        while timer != 6500:
-            if timer == 1500:
-                arret(1)
-                depose_tapis()
-            else:
-                timer = timer + 10
-            marche_avant(10)
+                self.timer = self.timer + 10
+            self.marche_avant(10)
+
 
 def main():
-    Roger = IaPMI()
+    IaPMI()
     print("Roger is Succesful !")
 
 if __name__ == '__main__':
