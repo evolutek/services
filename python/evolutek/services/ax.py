8826389123a1e19d11dce1a3c00a94f6cd0812e3
Fabien Siron <fabien.siron@epita.fr>
2016-05-19 14:22:49 +0000

    Merge self.dxl and libdxl variables

--- python/evolutek/services/ax.py
+++ python/evolutek/services/ax.py
@@ -32,18 +32,16 @@ class Ax(Service):
         super().__init__(identification=str(ax))
         self.ax = ax
 
-        libdxl = None
+        self.libdxl = None
         for path in LIBDXL_PATH:
             try:
-                libdxl = ctypes.CDLL(path + "/libdxl.so")
+                self.libdxl = ctypes.CDLL(path + "/libdxl.so")
             except:
                 pass
-        if not libdxl:
+        if not self.libdxl:
             raise RuntimeError("Cannot load libdxl.so, check LIBDXL_PATH")
-        self.dxl = libdxl
-        # TODO: rename self.dxl to self.libdxl
 
-        ret = self.dxl.dxl_initialize(DEVICE_ID, BAUD_RATE)
+        ret = self.libdxl.dxl_initialize(DEVICE_ID, BAUD_RATE)
         if ret != 1:
             raise RuntimeError("Cannot initialize device")
 
@@ -53,62 +51,62 @@ class Ax(Service):
 
     @Service.action
     def dxl_get_result(self):
-        return int(self.dxl.dxl_get_result())
+        return int(self.libdxl.dxl_get_result())
 
     @Service.action
     def move(self, goal):
-        return self.dxl.dxl_write_word(self.ax, AX_GOAL_POSITION_L, int(goal))
+        return self.libdxl.dxl_write_word(self.ax, AX_GOAL_POSITION_L, int(goal))
 
     @Service.action
     def get_present_position(self):
-        return self.dxl.dxl_read_word(self.ax, AX_PRESENT_POSTION_L)
+        return self.libdxl.dxl_read_word(self.ax, AX_PRESENT_POSTION_L)
 
     @Service.action
     def get_present_speed(self):
-        return self.dxl.dxl_read_word(self.ax, AX_PRESENT_SPEED_L)
+        return self.libdxl.dxl_read_word(self.ax, AX_PRESENT_SPEED_L)
 
     @Service.action
     def get_present_load(self):
-        return self.dxl.dxl_read_word(self.ax, AX_PRESENT_LOAD_L)
+        return self.libdxl.dxl_read_word(self.ax, AX_PRESENT_LOAD_L)
 
     @Service.action
     def get_present_voltage(self):
-        return self.dxl.dxl_read_byte(self.ax, AX_PRESENT_VOLTAGE)
+        return self.libdxl.dxl_read_byte(self.ax, AX_PRESENT_VOLTAGE)
 
     @Service.action
     def get_present_temperature(self):
-        return self.dxl.dxl_read_byte(self.ax, AX_PRESENT_TEMPERATURE)
+        return self.libdxl.dxl_read_byte(self.ax, AX_PRESENT_TEMPERATURE)
 
     @Service.action
     def get_cw_angle_limit(self):
-        return self.dxl.dxl_read_word(self.ax, AX_CW_ANGLE_LIMIT_L)
+        return self.libdxl.dxl_read_word(self.ax, AX_CW_ANGLE_LIMIT_L)
 
     @Service.action
     def get_ccw_angle_limit(self):
-        return self.dxl.dxl_read_word(self.ax, AX_CCW_ANGLE_LIMIT_L)
+        return self.libdxl.dxl_read_word(self.ax, AX_CCW_ANGLE_LIMIT_L)
 
     @Service.action
     def mode_wheel(self):
-        self.dxl.dxl_write_word(self.ax, AX_CW_ANGLE_LIMIT_L, 0)
-        return self.dxl.dxl_write_word(self.ax, AX_CCW_ANGLE_LIMIT_L, 0)
+        self.libdxl.dxl_write_word(self.ax, AX_CW_ANGLE_LIMIT_L, 0)
+        return self.libdxl.dxl_write_word(self.ax, AX_CCW_ANGLE_LIMIT_L, 0)
 
     @Service.action
     def mode_joint(self):
-        self.dxl.dxl_write_word(self.ax, AX_CW_ANGLE_LIMIT_L, 0)
-        return self.dxl.dxl_write_word(self.ax, AX_CCW_ANGLE_LIMIT_L, 1023)
+        self.libdxl.dxl_write_word(self.ax, AX_CW_ANGLE_LIMIT_L, 0)
+        return self.libdxl.dxl_write_word(self.ax, AX_CCW_ANGLE_LIMIT_L, 1023)
 
     @Service.action
     def moving_speed(self, speed):
-        return self.dxl.dxl_write_word(self.ax, AX_MOVING_SPEED_L, int(speed))
+        return self.libdxl.dxl_write_word(self.ax, AX_MOVING_SPEED_L, int(speed))
 
     @Service.action
     def turn(self, side: "1 or -1", speed):
-        self.dxl.dxl_write_word(self.ax, AX_MOVING_SPEED_L,
+        self.libdxl.dxl_write_word(self.ax, AX_MOVING_SPEED_L,
                                 (2**10 if int(side) == 1 else 0) | int(speed))
 
     @Service.action
     def free(self):
-        self.dxl.dxl_write_byte(self.ax, AX_TORQUE_ENABLE_B, 0)
+        self.libdxl.dxl_write_byte(self.ax, AX_TORQUE_ENABLE_B, 0)
 
 
 def main():
