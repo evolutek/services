0c78324308d52cd5e5ea9a3fc0aed36e0d4e28f8
Yohann Belair <belair_y@epita.fr>
2017-04-21 03:12:15 +0000

    sick gtb6 service

---
+++ python/evolutek/services/sick_gtb6_front.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import time
+
+from cellaserv.service import Service, ConfigVariable
+from evolutek.lib.settings import ROBOT
+from time import sleep
+import mraa
+
+class SicksFront(Service):
+
+    identification = ROBOT
+#change section "sharp" to "sick_gtb6"
+    period = ConfigVariable(section="sharp", option="period", coerc=float)
+
+    def __init__(self, sicks):
+        super().__init__()
+        for sick in sicks:
+            mraa.Gpio(sick).dir(mraa.DIR_IN)
+        self.sicks = sicks
+
+    def read(self, id):
+        return mraa.Gpio(id).read()
+
+    @Service.thread
+    def looping(self, *args, **kwargs):
+        while True:
+            time.sleep(period)
+#change publish message x)
+            for sick in sicks:
+                if(self.read(sick) == 1)
+                    self.publish("sharp_avoid")
+                    break
+
+def main():
+    sicks = [1, 2]
+    sicksfront = SicksFront(sicks)
+    Service.loop()
+
+if __name__ == "__main__":
+    main()
