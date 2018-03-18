d3189c3eef3694a096d16e2a39832e5767dc9fef
Corentin Vigourt <corentin.vigourt@epita.fr>
2018-03-18 18:21:41 +0100

    ai_pal: use task_maker

--- python/evolutek/services/task_maker.py
+++ python/evolutek/services/task_maker.py
@@ -17,14 +17,12 @@ class Task:
         self.action = func_ptr
         self.speed = speed
         self.not_avoid = not_avoid
-        
 
 
 def get_strat_orange():
     res = queue.Queue()
     res.put(Task(2700, 1000))
     res.put(Task(2400, 300))
-    
     return res
 
 
@@ -33,17 +31,14 @@ def get_strategy(filename, is_green = True):
     Read a file that contains the list of tasks (coordinates x, y, theta,
     func_id and boolean)
     and returns a queue of tasks
-    
     strat_file = open(filename, 'r')
     res = queue.Queue()
     line = strat_file.readline()
-    
     while line:
         instrs = line.split(';')
 	#todo : take into account the team color when adding objectives
         res.put(Task(int(instrs[0]), int(instrs[1]), int(instrs[2]), \
                  tasks.get_task(instrs[3]), instrs[4] == 't'))
-        line = strat_file.readline()    
-    
+        line = strat_file.readline()
     return res"""
     pass
