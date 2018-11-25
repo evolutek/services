b42be36adc99e0892d26666ead5bc6d477bbaaa6
Corentin Vigourt <corentin.vigourt@epita.fr>
2018-11-25 14:33:25 +0100

    Add Spi and Pwm support

--- python/evolutek/services/gpios.py
+++ python/evolutek/services/gpios.py
@@ -11,52 +11,80 @@ class Type(Enum):
         AIO = 0
         GPIO = 1
         PWM = 2
+        SPI = 3
+        UART = 4
 
-class Gpio(Service):
+class Io():
 
-    def __init__(self, id, name, type, dir):
+    def __init__(self, id, name, dir=True):
         self.id = id
         self.name = name
         self.value = None
         self.dir = dir
-        self.type = type
         self.port = None
         self.lock = Lock()
 
-        if type == Type.AIO:
-            self.port = mraa.Aio(id)
+    def read(self):
+        if self.dir:
+            return None
+        with self.lock:
+            self.value = self.port.read()
+        return self.value
 
-        if type == Type.GPIO:
-            self.port = mraa.Gpio(id)
+    def write(self, value):
+        if not self.dir:
+            return False
+        with self.lock:
+            self.port.write(int(value))
+        return True
+
+class Aio(Io):
+
+    def __init(self, id, name):
+        super().__init__(id, name, False)
+        self.port = mraa.Aio(id)
+
+    def read(self):
+        with self.lock:
+            return self.port.readFloat()
+
+class Gpio(Io):
+
+    def __init__(self, id, name, dir):
+        super().__init__(id, name, dir)
+        self.port = mraa.Gpio(id)
+        with lock:
             if dir:
                 self.port.dir(mraa.DIR_OUT)
             else:
                 self.port.dir(mraa.DIR_IN)
 
-        if type == Type.PWM:
-            self.port = mraa.Pwm(id)
+class Pwm(Io):
 
-    def read_value(self):
-        if self.dir:
-            return None
-
+    def __init__(self, id, name, dir, period, enable):
+        super().__init__(id, name, dir)
+        self.port = mraa.Pwm(id)
+        self.period = period
+        self.port.period_us(period)
+        self.port.enable(enable)
         with self.lock:
-            if self.type == Type.AIO:
-                self.value = self.port.ReadFloat()
-            if self.type == Type.GPIO or Type.PWM:
-                self.value.read()
+            if dir:
+                self.port.dir(mraa.DIR_OUT)
+            else:
+                self.port.dir(mraa.DIR_IN)
 
-        return self.value
+def Spi(Io):
 
-    def write_value(self, value):
-        if not self.dir or self.type == Type.AIO:
-            return False
+    def __init__(self, id, name):
+        super().__init__(id, name)
+        self.port = mraa.Spi(id)
 
-        with self.lock:
-            self.value = value
-            self.port.write(int(value))
+class Uart(Io):
 
-        return True
+    def __init__(self, id, name, baud_rate):
+        super().__init__(id, name, False)
+        self.port = mraa.Uart(id)
+        self.port.setBaudRate(baud_rate)
 
 class Gpios(Service):
 
@@ -64,61 +92,61 @@ class Gpios(Service):
         super().__init__(ROBOT)
         self.gpios = []
 
-    def add_gpio(self, id, name, type, dir):
-        self.gpios.append(Gpio(id, name, type, dir))
+    @Service.action
+    def add_gpio(self, type, id, name, dir=False, period=None, enable=None, baud_rate=None):
+        if type == Type.AIO:
+            self.gpios.append(Aio(id, name))
+        elif type == Type.GPIO:
+            self.gpios.append(Gpio(id, name, dir))
+        elif type == Type.PWM:
+            self.gpios.append(Pwm(id, name, dir, period, enable))
+        elif type == Type.SPI:
+            self.gpios.append(Spi(id, name))
+        elif type == Type.UART:
+            self.gpios.append(Uart(id, name, baud_rate))
 
     @Service.action
     def read_gpio(self, name=None, id=None):
         if name is None and id is None:
             return None
 
-        if name is None:
-            for gpio in self.gpios:
-                if gpio.id == id:
-                    return gpio.read_walue()
-
-        else:
-            for gpio in self.gpios:
-                if gpio.name == name:
-                    return gpio.read_value()
+        for gpio in self.gpios:
+            if (id != None and gpio.id == id) or (name != None and gpio.name == name):
+                return gpio.read()
 
         return None
 
     @Service.action
-    def write_gpio(self, name=None, id=None, value=None):
+    def write_gpio(self, value, name=None, id=None):
         if name is None and id is None:
             return False
 
-        if name is None:
-            for gpio in self.gpios:
-                if gpio.id == id:
-                    return gpio.write_value(value)
-
-        else:
-            for gpio in self.gpios:
-                if gpio.name == name:
-                    return gpio.write_value(value)
+        for gpio in self.gpios:
+            if (id != None and gpio.id == id) or (name != None and gpio.name == name):
+                return gpio.write(value)
 
         return False
 
     def update(self, refresh):
         while True:
             for gpio in self.gpios:
-                if gpio.dir:
+                if not gpio.dir:
                     tmp = gpio.value
-                    new = gpio.read_value()
+                    new = gpio.read()
                     if tmp != new:
                         self.publish("gpios", gpio.name, gpio.id, gpio.value)
-            sleep(refresh)
+            sleep(float(refresh))
 
 def main():
     gpios = Gpios()
-    gpios.add_gpio(9, "led", Type.GPIO, True)
+
+    # example
+    # gpios.add_gpio(type.GPIO, 9, "led", True)
 
     cs = CellaservProxy()
     auto = cs.config.get(section="gpios", option="auto")
 
-    if str(auto):
+    if bool(auto.decode()):
         refresh = cs.config.get(section="gpios", option="refresh")
         thread = Thread(target=gpios.update, args=[refresh])
         thread.start()
