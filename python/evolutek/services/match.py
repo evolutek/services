e569698c7b298231b0f26ddaa39699e1ab6f6044
Corentin Vigourt <corentin.vigourt@epita.fr>
2018-12-01 23:42:35 +0100

    Match services works

--- python/evolutek/services/match.py
+++ python/evolutek/services/match.py
@@ -1,22 +1,26 @@
 from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Service
-from evolutek.lib import ROBOT
-from threading import Timer
+from evolutek.lib.settings import ROBOT
+from threading import Timer, Lock, Thread
 from time import sleep
 
-@Service.require('ai', ROBOT)
+#@Service.require('ai', ROBOT)
 @Service.require('config')
-@Service.require('gpios', ROBOT)
-@Service.require('trajman', ROBOT)
+#@Service.require('gpios', ROBOT)
+#@Service.require('trajman', ROBOT)
 class Match(Service):
 
     def __init__(self):
 
+        self.cs = CellaservProxy()
         # Cellaserv services
-        self.ai = cs.ai[ROBOT]
-        self.config = cs.config
-        self.gpios = cs.gpios[ROBOT]
-        self.trajman = cs.trajman[ROBOT]
+        #self.ai = self.cs.ai[ROBOT]
+        self.config = self.cs.config
+        #self.gpios = self.cs.gpios[ROBOT]
+        #self.trajman = self.cs.trajman[ROBOT]
+
+        self.lock = Lock()
+        #self.update_thread = Thread(target=self.update_data)
 
         # Color params
         self.color1 = self.config.get(section='match', option='color1')
@@ -24,7 +28,7 @@ class Match(Service):
 
         # Match timer
         self.match_time = self.config.get(section='match', option='time')
-        self.timer = Timer(int(self.match_time), self.match_end)
+        self.timer = Timer(int(self.match_time) - 5, self.match_end)
 
         # Beacon param
         self.url = self.config.get(section='beacon', option='url')
@@ -35,60 +39,71 @@ class Match(Service):
 
         # Match params
         self.color = None
-        self.match_satus = 'unstarted'
+        self.match_status = 'unstarted'
         self.position = None
         self.direction = None
         self.robots = []
         self.front_detected = 0
         self.back_detected = 0
         self.score = 0
-        self.tirette = 0
+        self.tirette = False
+
+        #self.update_thread.start()
+
+        super().__init__(identification=ROBOT)
+
+        print('Match ready')
 
 
     # Pull robots positions
-    @Service.thread
+    @Service.thead
     def update_data(self):
+        print('lol')
         while True:
-            # Get robots
-            self.publish('robots', self.robots)
+            with self.lock:
+                # Get robots
+                self.publish('robots', self.robots)
 
-            # Update position
-            self.position = self.trajman.get_position()
-            self.publish('position', position)
+                # Update position
+                #self.position = self.trajman.get_position()
+                self.publish('position', self.position)
 
-            # Update moving direction
-            vector = self.get_vector_trsl()
-            if vector['trsl_vector'] > 0.0:
-                self.direction = True
-            elif vector['trsl_vector'] < 0.0:
-                self.direction = False
-            else:
-                self.direction = None
-            self.publish('direction', self.direction)
+                # Update moving direction
+                #vector = self.trajman.get_vector_trsl()
+                #if vector['trsl_vector'] > 0.0:
+                #    self.direction = True
+                #elif vector['trsl_vector'] < 0.0:
+                #    self.direction = False
+                #else:
+                #    self.direction = None
+                self.publish('direction', self.direction)
 
             sleep(self.refresh)
 
     # Start match
     def match_start(self):
-        self.ai.start()
+        print('match_start')
+        #self.ai.start()
         self.timer.start()
         self.match_status = 'started'
 
     # End match
     def match_end(self):
-        self.ai.end()
+        print('match_end')
+        #self.ai.end()
         self.match_status = 'ended'
 
     # Update score
     @Service.event
     def score(self, value):
-        self.score += value
+        self.score += int(value)
+        print('score is now: %d' % self.score)
 
     # Update front detection
     @Service.event
     def front(self, name, id, value):
         print('Front detection from: (%s, %s) with: %s' % (name, id, value))
-        if bool(value):
+        if int(value):
             if self.front_detected == 0:
                 self.publish('front_detection')
             self.front_detected += 1
@@ -102,8 +117,7 @@ class Match(Service):
     # Update back detection
     @Service.event
     def back(self, name, id, value):
-        print('Back detection from: (%s, %s) with: %s' % (name, id, value))
-        if bool(value):
+        if int(value):
             if self.back_detected == 0:
                 self.publish('back_detection')
             self.back_detected += 1
@@ -118,7 +132,7 @@ class Match(Service):
     @Service.event
     def tirette(self, name, id, value):
         print('Tirette: %s' % value)
-        self.tirette = bool(tirette)
+        self.tirette = int(value)
         if self.tirette:
             self.tirette_inserted =  True
             print('Tirette is inserted')
@@ -129,23 +143,26 @@ class Match(Service):
 
     @Service.event
     def reset(self):
-        self.color = self.color1 if int(self.gpios.read('color')) else self.color2
+        print('reset')
+        #self.color = self.color1 if int(self.gpios.read('color')) else self.color2
         self.match_status = 'unstarted'
         self.score = 0
         self.tirette_inserted = False
-        self.ai.reset(self.color)
+        #self.ai.reset(self.color)
 
     @Service.action
     def get_match(self):
-        self.match = {}
+        match = {}
         match['status'] = self.match_status
         match['color'] = self.color
-        match['pal'] = self.pal_position
-        match['pmi'] = self.pmi_position
+        match['direction'] = self.direction
+        match['position'] = self.position
         match['robots'] = self.robots
-        match['front'] = self.front_detect
-        match['back'] = self.back_detection
+        match['front'] = self.front_detected
+        match['back'] = self.back_detected
         match['score'] = self.score
+        match['tirette'] = self.tirette
+        return match
 
 def main():
     match = Match()
