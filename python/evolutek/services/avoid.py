25220fb53bd61930efc7d80c05ce49e3161174e4
Corentin Vigourt <corentin.vigourt@epita.fr>
2019-01-25 01:50:22 +0100

    Update avoid

--- python/evolutek/services/avoid.py
+++ python/evolutek/services/avoid.py
@@ -2,36 +2,70 @@
 
 from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Service
+from evolutek.lib.settings import ROBOT
 
-@Service.require("trajman", "pal")
+from time import sleep
+
+@Service.require("config")
+@Service.require("trajman", ROBOT)
+@Service.require("gpios", ROBOT)
 class Avoid(Service):
     def __init__(self):
-        super().__init__("pal")
         self.cs = CellaservProxy()
-        self.trajman = self.cs.trajman["pal"]
+        self.trajman = self.cs.trajman[ROBOT]
+        self.refresh = float(self.config.get(section='avoid', option='refresh'))
 
-        self.direction = 0.0
-        self.front_detected = False
-        self.back_detected = False
+        self.telemetry = None
+        self.front_detected = 0
+        self.back_detected = 0
+        self.avoid = False
 
-    @Service.event
+        super().__init__(ROBOT)
+
+    @Service.thread
+    def status(self):
+        while True:
+            s = {
+                'front_detected' : self.front_detected,
+                'back_detected' : self.back_detected,
+                'avoid' : self.avoid
+            }
+            self.publish(ROBOT + '_avoid_status', s)]
+            sleep(self.refresh)
+
+    @Service.thread
     def direction(self, value):
-        self.direction = float(value)
+        while True:
+            if self.telemetry is None:
+                continue
 
-        if self.direction > 0.0 and self.front_detected:
-            self.trajman.stop_asap(2000, 30)
-            print("[AVOID] Front detection")
-        elif self.direction < 0.0 and self.back_detected:
-            self.trajman.stop_asap(2000, 30)
-            print("[AVOID] Back detection")
+            if self.telemetry['speed'] > 0.0 and self.front_detected > 0:
+                self.trajman.stop_asap(2000, 30)
+                self.avoid = True
+                print("[AVOID] Front detection")
+            elif self.telemetry['speed'] and self.back_detected > 0:
+                self.trajman.stop_asap(2000, 30)
+                self.avoid = True
+                print("[AVOID] Back detection")
+            else:
+                self.avoid = False
+            slee(self.refresh)
 
     @Service.event
-    def front_detection(self, value):
-        self.front_detected = bool(value)
+    def front(self, name, id, value):
+        print('Front detection from: (%s, %s) with: %s' % (name, id, value))
+        if int(value):
+            self.front_detected += 1
+        else if self.front_detected > 0:
+            self.front_detected -= 1
 
+    # Update back detection
     @Service.event
-    def back_detection(self, value):
-        self.back_detected = bool(value)
+    def back(self, name, id, value):
+        if int(value):
+            self.back_detected += 1
+        elif self.back_detected > 0
+            self.back_detected -= 1
 
 def main():
     avoid = Avoid()
