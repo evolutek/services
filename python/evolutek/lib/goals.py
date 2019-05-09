67a9a6d12da695ebb9686e19e6082c3528c8ce3b
Corentin Vigourt <corentin.vigourt@epita.fr>
2019-05-09 12:53:12 +0200

    Fix Path parsing in goals Add Avoid Strategy Remove Point class (exists inside the lib) Add goal name

--- python/evolutek/lib/goals.py
+++ python/evolutek/lib/goals.py
@@ -1,6 +1,14 @@
+from enum import Enum
 from math import pi, sqrt
 #import pygraphviz as pgv
 import json
+
+from evolutek.lib.point import Point
+
+class Avoid(Enum):
+    Wait = 0
+    Timeout = 1
+    Skip = 2
 
 """
 class Node:
@@ -12,13 +20,18 @@ class Node:
         self.done = done
 """
 
+""" CLASS """
+
 class Action:
 
-    def __init__(self, fct, args=None, avoid=True, trsl_speed=None, rot_speed=None, mirror=False):
+    def __init__(self, fct, args=None, avoid=True, avoid_strategy=Avoid.Wait, trsl_speed=None, rot_speed=None, score=0, mirror=False):
         self.fct = fct
 
         if args and 'theta' in args and isinstance(args['theta'], str):
             args['theta'] = eval(args['theta'])
+
+        if isinstance(avoid_strategy, str):
+            avoid_strategy = eval(avoid_strategy)
 
         # mirror
         if args and mirror and 'y' in args:
@@ -28,8 +41,10 @@ class Action:
 
         self.args = args
         self.avoid = avoid
+        self.avoid_strategy = avoid_strategy
         self.trsl_speed = trsl_speed
         self.rot_speed = rot_speed
+        self.score = score
 
     def make(self):
         if not self.args is None:
@@ -42,16 +57,18 @@ class Action:
 
 class Goal:
 
-    def __init__(self, path, theta=None, actions=None, score=0, robot_proximity=False, priority=0, time=0, mirror=False):
-        #mirror
+    def __init__(self, name, path, theta=None, actions=None, score=0, robot_proximity=False, priority=0, time=0, mirror=False):
+        self.name = name
         self.path = []
-        lcl_theta = path.theta
-        if isinstance(theta, str):
-            lcl_theta = eval(theta)
-        if not theta is None and mirror:
-            lcl_theta = 0 - lcl_theta
-        lcl_theta = path.y if not mirror else 3000 - path.y
-        self.path.append(Point(path.x, path.y, theta=lcl_theta))
+        for point in path:
+            self.path.append(Point(point['x'], 3000 - point['y'] if mirror else point['y']))
+        #lcl_theta = path.theta
+        #if isinstance(theta, str):
+        #    lcl_theta = eval(theta)
+        #if not theta is None and mirror:
+        #    lcl_theta = 0 - lcl_theta
+        #lcl_theta = path.y if not mirror else 3000 - path.y
+        #self.path.append(Point(path.x, path.y, theta=lcl_theta))
 
         self.actions = [] if actions is None else actions
         self.score = score
@@ -64,14 +81,8 @@ class Goal:
         actions = ""
         for action in self.actions:
             actions += "->%s\n" % str(action)
-        return "x: %f\ny: %f\ntheta: %s\nscore: %d\nactions:\n%s"\
-            % (self.path[0].x, self.path[0].y, str(self.path[0].theta), self.score, actions)
-
-class Point:
-    def __init__(self, x, y, theta=None):
-        self.x = x
-        self.y = y
-        self.theta = theta
+        return "---%s---\nx: %f\ny: %f\nscore: %d\nactions:\n%s"\
+            % (self.name, self.path[0].x, self.path[0].y, self.score, actions)
 
 """ STRATS FOR TESTS """
 ##TODO: RM
@@ -135,6 +146,8 @@ def exp_strategy(cs, mirror=False):
     ], score=40))
     return l
 
+
+""" Goals manager """
 ##TODO: add name
 class Goals:
 
@@ -190,34 +203,47 @@ class Goals:
         for goal in goals['goals']:
 
             # Check if goal is correct
-            if not 'path' in goal:
-                print('Error in parsing goal: Missing path')
+            if not 'name' in goal:
+                print('Error in parsing goal: Missing name')
                 return False
+
+            if not 'path' in goal:
+                print('Error in parsing goal %s: Missing path' % goal['name'])
+                return False
+
+            for point in goal['path']:
+                if not 'x' in point:
+                    print('Error in parsing path in goal %s: Missing x' % goal['name'])
+                if not 'y' in point:
+                    print('Error in parsing path in goal %s: Missing y' % goal['name'])
 
             # Parse actions
             actions = []
             if 'actions' in goal:
                 for action in goal['actions']:
                     if action['name'] not in goals['actions']:
-                        print('Failed to get action: Missing action')
+                        print('Failed to get action in goal %s: Missing action' % goal['name'])
                         return False
                     action_instance = dict(goals['actions'][action['name']])
                     if not 'fct' in action_instance:
-                        print('Error in parsing action in goal: Missing fct')
+                        print('Error in parsing action in goal %s: Missing fct' % goal['name'])
                         return False
 
                     # Get instance of fct via CellaservProxy (cs.service['id'].fct)
                     try:
                         fct = getattr(getattr(self.cs, action_instance['service'])[action_instance['id']], action_instance['fct'])
                     except Exception as e:
-                        print('Failed to get fct: %s' % str(e))
+                        print('Failed to get fct in action %s: %s' % (action['name'], str(e)))
+
+                    # Remove service attributes
                     del action_instance['service']
                     del action_instance['id']
                     del action_instance['fct']
-                    if 'args' in action:
-                        actions.append(Action(fct, args=action['args'], **action_instance, mirror=mirror))
-                    else:
-                        actions.append(Action(fct, **action_instance, mirror=mirror))
+
+                    # Remove name insinde action
+                    del action['name']
+
+                    actions.append(Action(fct, **action, **action_instance, mirror=mirror))
 
             goal['actions'] = actions
             result.append(Goal(**goal, mirror=mirror))
@@ -229,12 +255,12 @@ class Goals:
         self.goals = []
         self.current = 0
         #self.goals = get_simple_strategy()
-        self.goals = test_avoid_strategy(mirror)
+        #self.goals = test_avoid_strategy(mirror)
         #self.goals = palet_strategy(self.cs, mirror)
         #self.goals = test_wall_evit(mirror)
         #self.goals = goldenium_strat(self.cs)
         #self.goals = exp_strategy(self.cs, mirror)
-        return True
+        #return True
 
         return self.parse(mirror)
 
@@ -280,14 +306,18 @@ class Goals:
 class fake_actuators:
     def __init__(self):
         self.get_palet = 'get_palet'
-        self.open_arms = 'open_arms'
-        self.close_arms = 'close_arms'
-        self.activate_exp = 'activate_exp'
+        self.push_ejecteur = 'push_ejecteur'
+        self.reset_ejecteur = 'reset_ejecteur'
+        self.drop_goldenium = 'drop_goldenium'
+        self.get_goldenium = 'get_goldenium'
+        self.get_blue_palet = 'get_blue_palet'
+        self.drop_blue_palet = 'drop_blue_palet'
 
 class fake_trajman:
     def __init__(self):
-        self.goto_xy = "goto_xy"
-        self.goto_theta = "goto_theta"
+        self.goto_xy = 'goto_xy'
+        self.goto_theta = 'goto_theta'
+        self.move_trsl = 'move_trsl'
 
 class fake_cs:
     def __init__(self):
