57b2392b34588c04cb2603c313872922b995211b
Corentin Vigourt <corentin.vigourt@epita.fr>
2019-05-22 03:01:49 +0200

    Make avoid compute tmp robot and gave it to AI

--- python/evolutek/services/avoid.py
+++ python/evolutek/services/avoid.py
@@ -2,17 +2,33 @@
 
 from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Service
+from evolutek.lib.point import Point
 from evolutek.lib.settings import ROBOT
 
+from math import cos, sin, tan, pi, sqrt
 import os
 from time import sleep
+
+dist_sensor_x = 115
+dist_sensor_y = 125
+
+sensors = {
+    'gtb1': {'x': dist_sensor_x, 'y': -dist_sensor_y},
+    'gtb2': {'x': dist_sensor_x, 'y': 0},
+    'gtb3': {'x': dist_sensor_x, 'y': dist_sensor_y},
+    'gtb4': {'x': -dist_sensor_x, 'y': -dist_sensor_y},
+    'gtb5': {'x': -dist_sensor_x, 'y': 0},
+    'gtb6': {'x': -dist_sensor_x, 'y': dist_sensor_y}
+}
 
 @Service.require("config")
 @Service.require("trajman", ROBOT)
 class Avoid(Service):
     def __init__(self):
         self.cs = CellaservProxy()
+
         self.refresh = float(self.cs.config.get(section='avoid', option='refresh'))
+        self.robot_dist_sensor = int(self.cs.config.get(section=ROBOT, option='dist_detection'))
 
         self.telemetry = None
         self.front_detected = []
@@ -41,8 +57,6 @@ class Avoid(Service):
             if not self.enabled:
                 continue
 
-
-            ## TODO Before stop, check if it is normal if a robot is in front of us
             if self.telemetry['speed'] > 0.0 and len(self.front_detected) > 0:
                 self.stop_robot('front')
                 print("[AVOID] Front detection")
@@ -57,10 +71,11 @@ class Avoid(Service):
     def stop_robot(self, side=None):
         print('----- Aborting: %s ---' % side)
 
-        ##TODO: compute pos of tmp robot
         try:
             self.cs.trajman[ROBOT].stop_asap(1000, 20)
-            self.cs.ai[ROBOT].abort(side=side)
+            sensor = self.compute_sensor_pos(side)
+            tmp_robot = self.compute_tmp_robot(sensor.to_dict(), side)
+            self.cs.ai[ROBOT].abort(side=side, tmp_robot=tmp_robot.to_dict())
         except Exception as e:
             print('[AVOID] Failed to abort ai of %s: %s' % (ROBOT, str(e)))
             return
@@ -97,6 +112,67 @@ class Avoid(Service):
             self.telemetry = None
         else:
             self.telemetry = telemetry
+
+    @Service.action
+    def compute_sensor_pos(self, side):
+        pos = self.cs.trajman[ROBOT].get_position()
+        stat = self.status()
+
+        s = []
+
+        for sensor in stat[side]:
+            sensor_pos = sensors[sensor]
+            s.append(Point(pos['x'] + sensor_pos['x'], pos['y'] + sensor_pos['y']))
+
+        cos_val = cos(pos['theta'])
+        sin_val = sin(pos['theta'])
+
+        new_s = []
+        for sensor in s:
+            new_s.append(Point(
+            (sensor.x - pos['x']) * cos_val - (sensor.y - pos['y']) * sin_val + pos['x'],
+            (sensor.x - pos['x']) * sin_val + (sensor.y - pos['y']) * cos_val + pos['y']
+            ))
+
+        return Point.mean(new_s)
+
+    @Service.action
+    def compute_tmp_robot(self, pos, side):
+
+        # We use theta between 0 and 2pi
+        theta = self.cs.trajman[ROBOT].get_position()['theta']
+        if theta < 0:
+            theta += 2 * pi
+
+        y = False
+        m = 0
+        n = 0
+        delta = 0.1
+        if abs(pi/2 - theta) < delta or abs(3*pi/2 - theta) < delta:
+            y = True
+            m = tan((pi/2) - theta)
+            n = pos['x'] - (m * pos['y'])
+        else:
+            m = tan(theta)
+            n = pos['y'] - (m * pos['x'])
+
+        sens = theta > pi / 2 and theta < 3 * pi / 2 if not y else theta > pi
+
+        dist = (self.robot_dist_sensor / 2 + 210) / sqrt(1 + m ** 2)
+
+        if sens ^ (side != 'front'):
+            dist *= -1
+
+        new_x = 0
+        new_y = 0
+        if y:
+            new_y = int(pos['y'] + dist)
+            new_x = int(new_y * m + n)
+        else:
+            new_x = int(pos['x'] + dist)
+            new_y = int(new_x * m + n)
+
+        return Point(new_x, new_y)
 
 def wait_for_beacon():
     hostname = "pi"
