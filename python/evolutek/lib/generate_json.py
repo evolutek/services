1cdc79b40b5a6a062409ffd35ad5c99fd733e1fe
Andre Debuisne <andre.debuisne@epita.fr>
2019-05-28 05:17:39 +0200

    [AI] CDR 2019 strategies

--- python/evolutek/lib/generate_json.py
+++ python/evolutek/lib/generate_json.py
@@ -60,7 +60,9 @@ fg.add_action("init_ejecteur", "actuators", "pal", "init_ejecteur", False)
 fg.add_action("drop_goldenium", "actuators", "pal", "drop_goldenium", False)
 fg.add_action("get_goldenium", "actuators", "pal", "get_goldenium", False)
 fg.add_action("get_blue_palet", "actuators", "pal", "get_blue_palet", False)
+fg.add_action("get_palet", "actuators", "pal", "get_palet", False)
 fg.add_action("drop_blue_palet", "actuators", "pal", "drop_blue_palet", False)
+fg.add_action("drop_palet", "actuators", "pal", "drop_palet", False)
 fg.add_action("goto_xy_with_avoid", "trajman", "pal", "goto_xy", True)
 fg.add_action("goto_xy_without_avoid", "trajman", "pal", "goto_xy", False)
 fg.add_action("goto_theta_with_avoid", "trajman", "pal", "goto_theta")
@@ -69,6 +71,7 @@ fg.add_action("move_trsl", "trajman", "pal", "move_trsl", False)
 fg.add_action("set_x", "trajman", "pal", "set_x", False)
 fg.add_action("set_y", "trajman", "pal", "set_y", False)
 fg.add_action("set_theta", "trajman", "pal", "set_theta", False)
+fg.add_action("free", "trajman", "pal", "free", False)
 
 #fg.add_goal("Test", 0)\
 #    .add_path(600, 1200)\
@@ -90,25 +93,56 @@ fg.add_action("set_theta", "trajman", "pal", "set_theta", False)
 fg.add_goal("Push Front Palet", 0)\
     .add_path(1325, 225)\
     .add_goto_xy_subaction(1325, 500, False)\
-    .add_goto_xy_subaction(875, 500, True)
+    .add_goto_xy_subaction(850, 500, False)
+
+fg.add_goal("Push Chaos Zone", 20)\
+    .add_path(750, 1400)\
+    .add_goto_xy_subaction(1200, 1400, True)\
+    .add_goto_xy_subaction(1200, 1200, False)\
+    .add_goto_xy_subaction(615, 375, True)
+
+"""
+fg.add_goal("Get palets", 0)\
+    .add_path(1840, 225)\
+    .add_subaction("move_trsl", {"dest": 50, "acc": 100, "dec": 100, "maxspeed":500, "sens": 1})\
+    .add_subaction("free")\
+    .add_subaction("set_x", {"x": 1880})\
+    .add_subaction("move_trsl", {"dest": 100, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
+    .add_goto_theta_subaction("pi/2", False)\
+    .add_subaction("move_trsl", {"dest": 50, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
+    .add_subaction("free")\
+    .add_subaction("set_y", {"y": 130})\
+    .add_subaction("set_theta", {"theta": "pi/2"})\
+    .add_subaction("move_trsl", {"dest": 50, "acc": 100, "dec": 100, "maxspeed":500, "sens": 1})\
+    .add_goto_xy_subaction(1840, 225, False)\
+    .add_goto_theta_subaction(0, False)\
+    .add_subaction("get_palet")\
+    .add_goto_xy_subaction(1285, 225, False)\
+"""
 
 fg.add_goal("Get blue palet and drop it", 12)\
-    .add_path(1000, 500)\
-    .add_path(1235, 800)\
+    .add_path(1265, 800)\
     .add_goto_theta_subaction("0", False)\
     .add_subaction("get_blue_palet")\
-    .add_goto_xy_subaction(1160, 1320)\
-    .add_goto_theta_subaction("0", False)\
-    .add_goto_xy_subaction(1270, 1320, False)\
-    .add_subaction("drop_blue_palet")
+    .add_goto_xy_subaction(1380, 1320, False)\
+    .add_goto_theta_subaction("pi/2", False)\
+    .add_subaction("move_trsl", {"dest": 60, "acc": 100, "dec": 100, "maxspeed":500, "sens": 1})\
+    .add_subaction("free")\
+    .add_subaction("set_y", {"y": 1360})\
+    .add_subaction("set_theta", {"theta": "pi/2"})\
+    .add_subaction("move_trsl", {"dest": 70, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
+    .add_goto_theta_subaction("pi/6", False)\
+    .add_subaction("drop_blue_palet")\
+    .add_subaction("move_trsl", {"dest": 75, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
 
-fg.add_goal("Push Blue Palet", 20)\
+fg.add_goal("Push Blue Palet", 70)\
     .add_path(1000, 1320)\
-    .add_goto_xy_subaction(500, 1625, True)\
-    .add_goto_xy_subaction(150, 1625, False)\
+    .add_goto_xy_subaction(600, 1620, True)\
+    .add_goto_xy_subaction(150, 1620, False)\
     .add_subaction("move_trsl", {"dest": 60, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
     .add_subaction("push_ejecteur")\
-    .add_subaction("init_ejecteur")\
+    .add_subaction("reset_ejecteur")\
+    .add_subaction("free")\
     .add_subaction("set_x", {"x": 150})\
     .add_subaction("set_theta", {"theta": "0"})\
     .add_subaction("move_trsl", {"dest": 60, "acc": 100, "dec": 100, "maxspeed":500, "sens": 1})\
@@ -120,25 +154,24 @@ fg.add_goal("Push Blue Palet", 20)\
 #    .add_goto_xy_subaction(1325, 500, True)\
 #    .add_goto_xy_subaction(800, 225, True)
 
-#fg.add_goal("Push Chaos Zone", 17)\
-#    .add_path(800, 1350)\
-#    .add_goto_xy_subaction(1200, 1350, False)\
-#    .add_goto_xy_subaction(600, 375, True)
-
 
 fg.add_goal("Get Goldenium", 20)\
-    .add_path(450, 2235)\
+    .add_path(450, 2240)\
     .add_goto_theta_subaction("pi", False)\
-    .add_goto_xy_subaction(235, 2235, False)\
+    .add_goto_xy_subaction(235, 2225, False)\
     .add_subaction("get_goldenium", avoid="Avoid.Skip", score=20)\
-    .add_goto_xy_subaction(500, 2235, True)
+    .add_goto_xy_subaction(500, 2225, True)
 
 fg.add_goal("Drop Goldenium", 24)\
-    .add_path(1050, 1340)\
+    .add_path(1050, 1330)\
     .add_goto_theta_subaction("0", False)\
-    .add_goto_xy_subaction(1435, 1355, False)\
+    .add_goto_xy_subaction(1425, 1330, False)\
     .add_subaction("drop_goldenium")\
+    .add_subaction("move_trsl", {"dest": 60, "acc": 100, "dec": 100, "maxspeed":500, "sens": 0})\
 
-
+fg.add_goal("Push leftover palets", 0)\
+    .add_goto_xy_subaction(750, 575)\
+    .add_goto_xy_subaction(575, 575)\
+    .add_goto_theta_subaction("pi/2")
 
 fg.generate_file("test.json")
