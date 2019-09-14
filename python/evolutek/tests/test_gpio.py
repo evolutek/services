bac21604d925bb3709ce79c4b09721a35537c389
Corentin Vigourt <corentin.vigourt@epita.fr>
2019-09-14 16:16:25 +0200

    Add Gpio lib and tests

---
+++ python/evolutek/tests/test_gpio.py
@@ -0,0 +1,25 @@
+from cellaserv.service import Service
+from evolutek.lib.gpio import Gpio
+from time import sleep
+
+class Test_Gpio(Service):
+
+    def __init__(self):
+        self.gpio = Gpio(42, "my_gpio", dir=False, event="Auto refresh")
+
+        super().__init__()
+
+        self.gpio.auto_refresh(1, self)
+
+    #@Service.thread
+    def loop(self):
+        while True:
+            print("Loop read: %d" % self.gpio.read())
+            sleep(0.5)
+
+def main():
+    test_gpio = Test_Gpio()
+    test_gpio.run()
+
+if __name__ == "__main__":
+    main()
