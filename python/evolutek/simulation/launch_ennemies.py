f5b1f979ae0f00975874b3c945aeffbb492e73b5
Corentin Vigourt <corentin.vigourt@epita.fr>
2020-04-23 00:31:42 +0200

    Make enemies launch more simple and usable

--- python/evolutek/simulation/launch_ennemies.py
+++ python/evolutek/simulation/launch_ennemies.py
@@ -9,43 +9,48 @@ from sys import argv
 from time import sleep
 
 from cellaserv.proxy import CellaservProxy
-from evolutek.simulation.launch_robot import Robot, read_config
+from evolutek.simulation.launch_robot import Robot, read_config, Simulation
 
 #TODO Control Interface
 
-class Ennemie(Robot):
+class Enemy(Robot):
 
-    def __init__(self, ennemie):
+    def __init__(self, enemy, to_launch):
 
-        config = ennemie[1]['config']
+        print(f'[SIMULATION] Launching enemy {enemy[0]}')
+
+        config = enemy[1]['config']
 
         cs = CellaservProxy()
         for option, value in config.items():
-            cs.config.set_tmp(section=ennemie[0], option=option, value=value)
+            cs.config.set_tmp(section=enemy[0], option=option, value=value)
 
-        super().__init__(ennemie[0], ennemie[1]['scripts'])
+        super().__init__(enemy[0], {'service': to_launch})
 
-        return
+        sleep(2)
 
-        theta = ennemie[1]['theta']
+        theta = enemy[1]['theta']
         if isinstance(theta, str):
             theta = eval(theta)
 
         from evolutek.lib.robot import Robot as LibRobot
         robot = LibRobot()
-        robot.set_pos(ennemie[1]['x'], ennemie[1]['y'], theta)
+        robot.set_pos(enemy[1]['x'], enemy[1]['y'], theta)
 
 
 def main():
 
-    print('[SIMULATOR] Starting ennemies')
+    print('[SIMULATOR] Starting enemies')
 
-    ennemies = read_config('ennemies')
-    if ennemies is None:
+    enemies = read_config('enemies')
+    if enemies is None:
         return
 
-    for ennemie in ennemies.items():
-        Ennemie(ennemie)
+    for enemy in enemies['robots'].items():
+        Enemy(enemy, enemies['service'])
+
+    if len(enemies['scripts']) > 0:
+        Simulation(enemies['scripts'])
 
     while True:
         pass
