4d190d52417a986e1025358561a7db672a732247
Corentin Vigourt <corentin.vigourt@epita.fr>
2020-04-29 23:48:55 +0200

    Add calibration

--- python/evolutek/lib/rgb_sensors.py
+++ python/evolutek/lib/rgb_sensors.py
@@ -2,12 +2,11 @@ import adafruit_tca9548a
 import adafruit_tcs34725
 import board
 import busio
-
 import time
 
 class RGBSensors:
 
-    def __init__(self, sensors):
+    def __init__(self, sensors, sample_size):
 
         print('[RGB_SENSORS] Init RGB Sensors')
 
@@ -15,6 +14,7 @@ class RGBSensors:
         self.tca = adafruit_tca9548a.TCA9548A(i2c)
 
         self.sensors = {}
+        self.calibrations = {}
 
         for i in sensors:
             print('[RGB_SENSORS] Initializing RGB sensor %d' % i)
@@ -22,8 +22,31 @@ class RGBSensors:
                 print('[RGB_SENSORS] Bad RGB sensor number %d' % i)
                 continue
             self.sensors[i] = adafruit_tcs34725.TCS34725(self.tca[i - 1])
+            self.calibration(i, sample_size)
 
         print('[RGB_SENSORS] RGB Sensors initialized')
+
+    def __str__(self):
+        s = '---RGB Sensors:---\n'
+        for key in self.sensors:
+            s += 'sensor %d with calibration %s\n' % (key, self.calibrations[i])
+        return s
+
+    def calibration(self, i, sample_size):
+        print('[RGB_SENSORS] Calibrating RGB sensor %d' % i)
+        if not i in self.sensors:
+            print('[RGB_SENSORS] Bad RGB sensor number %d' % i)
+            return None
+
+        r, g, b = 0, 0, 0
+        for i in range(sample_size):
+            rgb = self.read_sensor(i)
+            r += rgb[0]
+            g += rgb[1]
+            b += rgb[2]
+            time.sleep(0.1)
+
+        self.calibrations[i] = (r // sample_size, g // sample_size, b // sample_size)
 
     def read_sensor(self, i):
         print('[RGB_SENSORS] Reading RGB sensor %d' % i)
@@ -31,10 +54,3 @@ class RGBSensors:
             print('[RGB_SENSORS] Bad RGB sensor  number %d' % i)
             return None
         return self.sensors[i].color_rgb_bytes
-
-    def read_color(self, i):
-        print('[RGB_SENSORS] Reading RGB color %d' % i)
-        if not i in self.sensors:
-            print('[RGB_SENSORS] Bad RGB sensor  number %d' % i)
-            return None
-        return self.sensors[i].color
--- python/evolutek/tests/test_rgb_sensors.py
+++ python/evolutek/tests/test_rgb_sensors.py
@@ -5,6 +5,8 @@ from evolutek.lib.rgb_sensors import RGBSensors
 
 rgb = RGBSensors([2, 3])
 
+print(rgb)
+
 while True:
     print('Sensor 2 Color: ({0}, {1}, {2})'.format(*rgb.read_sensor(2)))
     print('Sensor 2 Color: %s)' % str(rgb.read_color(2)))
