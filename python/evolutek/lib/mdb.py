92ff30ae7dfadcbc9af5b668931b259671e5fc7f
Speddy <yann-arthur.tcheumani@epitech.eu>
2020-06-26 21:29:12 +0200

    S/[add] prototype mdb position

--- python/evolutek/lib/mdb.py
+++ python/evolutek/lib/mdb.py
@@ -41,7 +41,8 @@ MAX_DIST = 1000
 
 class Mdb:
 
-    def __init__(self, nb_sensors, sensor_address=0x29, expander_address=0x20, leds_gpio=None, refresh=0.1, debug=0):
+    def __init__(self, nb_sensors, sensor_address=0x29, front_led=[16, 1, 2, 3], right_led=[4, 5, 6, 7], back_led=[8, 9, 10, 11],
+                 left_led=[12, 13, 14, 15], expander_address=0x20, leds_gpio=None, refresh=0.1, debug=0):
         self.coef = 255 / (MAX_DIST / 2)
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.sensors = []
@@ -52,6 +53,7 @@ class Mdb:
         self.pixel = None
         self.debug = debug
         self.sensors_address = sensor_address
+        self.position = {"back": (False, back_led), "front": (False, front_led),  "right": (False, right_led), "left": (False, left_led)}
         self.init_sensor()
         self.lock = Lock()
         Thread(target=self.loop).run()
@@ -65,7 +67,6 @@ class Mdb:
             self.mcp.get_pin(i + 8).value = True
             self.sensors.append(adafruit_vl53l0x.VL53L0X(self.i2c, address=0x29))
             self.sensors[i].set_address(0x29 + i + 1)
-
         self.pixel = neopixel.NeoPixel(board.D27, self.nb_capteur, brightness=10)
 
     def loop(self):
@@ -74,6 +75,22 @@ class Mdb:
                 self.dist = self.sensors[i].range
                 if self.dist > MAX_DIST:
                     self.pixel[i] = (0,0,0)
+                elif self.dist > 500:
+                    if i in self.position["front"][1]:
+                        self.position["front"][0] = True
+                    elif i in self.position["right"][1]:
+                        self.position["right"][0] = True
+                    elif i in self.position["back"][1]:
+                        self.position["back"][0] = True
+                    else:
+                        self.position["left"] = True
+
+                    r = 255 - self.dist * self.coef
+                    g = 255 - abs(MAX_DIST / 2 - self.dist) * self.coef
+                    b = 255 - (MAX_DIST - self.dist) * self.coef
+                    self.pixel[i] = (int(r if r >= 0 else 0),
+                                     int(g if g >= 0 else 0),
+                                     int(b if b >= 0 else 0))
                 else:
                     r = 255 - self.dist * self.coef
                     g = 255 - abs(MAX_DIST / 2 - self.dist) * self.coef
