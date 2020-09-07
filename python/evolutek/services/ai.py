708c1d9561d44d35898244c95c0f69c52ae52da1
Maxime Chardon <maxime.chardon@epita.fr>
2020-09-07 19:39:59 +0200

    [OK] Gestion tirette

--- python/evolutek/services/ai.py
+++ python/evolutek/services/ai.py
@@ -45,7 +45,7 @@ class Ai(Service):
         self.fsm.add_state(States.Ending, self.ending, prevs=[States.Setup, States.Waiting, States.Selecting, States.Making])
         self.fsm.add_error_state(self.error)
 
-        Gpio(5, "tirette", False, edge=Edge.FALLING).auto_refresh(service=self.publish)
+        Gpio(17, "tirette", False, edge=Edge.FALLING).auto_refresh(callback=self.publish)
 
         # TODO : change strat file
         #self.goals = Goals(file='/etc/conf.d/strategy-%s.json' % ROBOT, ai=self)
