b991f3ed5be23853460c3c416b8596591948c896
Rémi Audebert <rflah0@gmail.com>
2020-12-21 17:09:37 +0100

    services: test_config: fix

---
+++ python/evolutek/conftest.py
@@ -0,0 +1,4 @@
+# pytest default imports, used to provide fixtures
+from cellaserv.examples.conftest import cs
+
+_ = cs  # used in tests
--- python/evolutek/services/test_config.py
+++ python/evolutek/services/test_config.py
@@ -1,7 +1,6 @@
 import pytest
 
 from cellaserv.client import ReplyError
-from cellaserv.examples.fixtures import proxy
 from evolutek.services.config import Config
 
 
@@ -22,13 +21,13 @@ b = foobar
 
 
 @pytest.mark.asyncio
-async def test_service(config_service, proxy):
-    assert await proxy.config.get(section="a", option="b") == "foobar"
-    assert await proxy.config.get_section(section="a") == {"b": "foobar"}
+async def test_service(config_service, cs):
+    assert await cs.config.get(section="a", option="b") == "foobar"
+    assert await cs.config.get_section(section="a") == {"b": "foobar"}
 
     # Overwrite option
-    assert await proxy.config.set(section="a", option="b", value="c") is None
-    assert await proxy.config.get(section="a", option="b") == "c"
+    assert await cs.config.set(section="a", option="b", value="c") is None
+    assert await cs.config.get(section="a", option="b") == "c"
 
     # Verify that config writes to file
     assert (
@@ -40,9 +39,9 @@ b = c
     )
 
     # Test tmp set
-    assert await proxy.config.set_tmp(section="a", option="b", value="d") is None
+    assert await cs.config.set_tmp(section="a", option="b", value="d") is None
     # Get returns the new value
-    assert await proxy.config.get(section="a", option="b") == "d"
+    assert await cs.config.get(section="a", option="b") == "d"
     # Config file config has not changed
     assert (
         config_service.config_file_path.read_text()
@@ -53,17 +52,17 @@ b = c
     )
 
     # Test creating a new section
-    assert await proxy.config.set(section="foo", option="bar", value="test") is None
-    assert await proxy.config.get_section(section="foo") == {"bar": "test"}
+    assert await cs.config.set(section="foo", option="bar", value="test") is None
+    assert await cs.config.get_section(section="foo") == {"bar": "test"}
 
     # Test list()
-    assert await proxy.config.list() == {"a": {"b": "c"}, "foo": {"bar": "test"}}
+    assert await cs.config.list() == {"a": {"b": "c"}, "foo": {"bar": "test"}}
 
     # Test errors
     with pytest.raises(ReplyError):
-        await proxy.config.get(section="a", option="not_existing_option")
+        await cs.config.get(section="a", option="not_existing_option")
 
     with pytest.raises(ReplyError):
-        await proxy.config.get(
+        await cs.config.get(
             section="not_existing_section", option="not_existing_option"
         )
