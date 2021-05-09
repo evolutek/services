d61079e7fcdb1ff72608d1e76c528a2244541428
Corentin Vigourt <corentin.vigourt@epita.fr>
2021-05-09 02:33:16 +0200

    Update RGB sensors class

--- python/evolutek/lib/sensors/rgb_sensors.py
+++ python/evolutek/lib/sensors/rgb_sensors.py
@@ -4,59 +4,115 @@ import board
 import busio
 import time
 
-class TCS34725:
+from enum import Enum
+from time import sleep
 
-    def __init__(self, id):
-        self.id = id
+from evolutek.lib.component import Component, ComponentsHolder
+
+TCA = None
+CALLIBRATE = 10
+SENSITIVITY = 1.2
+
+class Colors(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+    NONE = None
+
+class TCS34725(Component):
+
+    def __init__(self, id, channel):
+        self.callibration = [0, 0, 0]
+        self.sensor = None
+        self.channel = channel
+        super().__init__('TCS34725', id)
+
+    def _initialize(self):
+        if self.id < 1 or self.id > 8:
+            print('[%s] %s bad id %d' % (self.name, self.name, self.id))
+            return False
 
         try:
-            self.sensor = adafruit_tcs34725.TCS34725(self.id - 1)
-        except:
-            raise RuntimeError("[TCS34725] Failed to initialize TCS34725 %d" % self.id)
+            self.sensor = adafruit_tcs34725.TCS34725(TCA[self.channel - 1])
+        except Exception as e:
+            print('[%s] Failed to initialize TCS34725 %d: %s' % (self.name, self.id, str(e)))
+            return False
+        return True
 
-        print('[TCS34725] TCS34725 %d initialized' % self.id)
+    def setup(self):
+        for i in range(CALLIBRATE):
+            rgb = self.sensor.color_rgb_bytes
+            self.callibration[0] += rgb[0]
+            self.callibration[1] += rgb[1]
+            self.callibration[2] += rgb[2]
+            sleep(0.1)
+        self.callibration[0] /= CALLIBRATE
+        self.callibration[1] /= CALLIBRATE
+        self.callibration[2] /= CALLIBRATE
+        # print('Setup: R = %i - G = %i - B = %i' % (self.callibration[0],self.callibration[1],self.callibration[2]))
 
-    def __str__(self):
-        # TODO
-        return ""
+    def isMajorChanges(self, rgb):
+        # print('R = %i - G = %i - B = %i' % (rgb))
+        if (rgb[0] >= self.callibration[0] * SENSITIVITY) \
+            or (rgb[1] >= self.callibration[1] * SENSITIVITY) \
+            or (rgb[2] >= self.callibration[2] * SENSITIVITY):
+            return True
+        return False
 
     def read(self):
-        return self.sensor.color_rgb_bytes
+        if not self.is_initialized:
+            print('[%s] %s %d not initialized' % (self.name, self.name, self.id))
+            return None
+        rgb = self.sensor.color_rgb_bytes
+        r, g, b = rgb[0], rgb[1], rgb[2]
+        colorByte = max(r, g, b)
+        if not self.isMajorChanges(rgb):
+            return Colors.NONE
+        elif colorByte == r:
+            return Colors.RED
+        elif colorByte == g:
+            return Colors.GREEN
+        else:
+            return Colors.BLUE
 
+    def __str__(self):
+        s = "----------\n"
+        s += "TCS34725: %d\n" % self.id
+        s += "Channel: %d\n" % self.channel
+        s += "Color: %s\n" % str(self.read())
+        s += "---------"
+        return s
 
-class RGBSensors:
+class RGBSensors(ComponentsHolder):
 
     def __init__(self, sensors):
 
-        print('[RGB_SENSORS] Init RGB Sensors')
+        if isinstance(sensors, list):
+            tmp = {}
+            for sensor in sensors:
+                tmp[sensor] = [sensor]
+            sensors = tmp
 
+        super().__init__('RGB sensors', sensors, TCS34725)
+
+    def _initialize(self):
         try:
             i2c = busio.I2C(board.SCL, board.SDA)
         except:
-            raise RuntimeError("[RGB_SENSORS] Failed to open I2C bus")
+            print('[%s] Failed to open I2C bus' % self.name)
+            raise False
 
         try:
-            self.tca = adafruit_tca9548a.TCA9548A(i2c)
+            global TCA
+            TCA = adafruit_tca9548a.TCA9548A(i2c)
         except:
-            raise RuntimeError("[RGB_SENSORS] Failed to initialize TCA")
+            print('[%s] Failed to initialize TCA' % self.name)
+            return False
 
-        self.sensors = {}
+        return True
 
-        for id in sensors:
-            if i < 1 or i > 8:
-                print('[RGB_SENSORS] Bad RGB sensor number %d' % i)
-                continue
-            self.sensors[i] = TCS34725(id)
-
-    def __str__(self):
-        # TODO
-        return ""
-
-    def __iter__(self):
-        return iter(self.sensors)
-
-    def __getitem__(self, key):
-        if key not in self.sensors:
-            print('[RGB_SENSORS] RGB sensor %d not registered' % key)
-            return None
-        return self.sensors[key]
+    def read_all_sensors(self):
+        results = {}
+        for sensor in self.components:
+            results[sensor] = self.components[sensor].read()
+        return results
---
+++ python/evolutek/tests/sensors/test_rgb_sensors.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+from evolutek.lib.sensors.rgb_sensors import RGBSensors
+
+rgb_sensors = RGBSensors([1, 2, 3, 4])
+print(rgb_sensors.is_initialized())
+print(rgb_sensors)
+
+for sensor in rgb_sensors:
+    rgb_sensors[sensor].setup()
+
+while True:
+    for sensor in rgb_sensors:
+        print('Sensor %s Color: (%s)' % (sensor, rgb_sensors[sensor].read().value))
+    print("\n---------------------------------------\n")
+    sleep(2)
