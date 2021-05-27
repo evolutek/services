2b68d0acb4def2686b588eccdc5860cd52b060fc
Corentin Vigourt <cvigourt@scallog.com>
2021-05-27 17:13:41 +0200

    WIP robot

--- python/evolutek/services/robot.py
+++ python/evolutek/services/robot.py
@@ -4,13 +4,15 @@ from cellaserv.proxy import CellaservProxy
 from cellaserv.service import Event as CellaservEvent, Service
 
 from evolutek.lib.map.point import Point
-from evolutek.lib.robot.robot_trajman import *
+import evolutek.lib.robot.robot_trajman as rt
 from evolutek.lib.sensors.rplidar import Rplidar
 from evolutek.lib.settings import ROBOT
-from evolutek.lib.utils.wrappers import event_waiter
+from evolutek.lib.status import RobotStatus
+from evolutek.lib.utils.wrappers import event_waiter, if_enabled
 
+from queue import Queue
 from time import time, sleep
-from threading import Lock
+from threading import Event, Lock
 
 @Service.require('config')
 @Service.require('actuators', ROBOT)
@@ -20,11 +22,18 @@ class Robot(Service):
     start_event = CellaservEvent('%s_started' % ROBOT)
     stop_event = CellaservEvent('%s_stopped' % ROBOT)
 
-    goto_xy = event_waiter(self.trajman.goto_xy)
-    goto_theta = event_waiter(self.trajman.goto_theta)
-    move_trsl = event_waiter(self.trajman.move_trsl)
-    move_rot = event_waiter(self.trajman.move_rot)
-    recal = event_waiter(self.recalibration)
+    goto_xy = Service.action(event_waiter(self.trajman.goto_xy))
+    goto_theta = Service.action(event_waiter(self.trajman.goto_theta))
+    move_trsl = Service.action(event_waiter(self.trajman.move_trsl))
+    move_rot = Service.action(event_waiter(self.trajman.move_rot)))
+    recal = Service.action(event_waiter(self.recalibration))
+
+    # Imported from root_trajman
+    set_x = Service.action(rt.set_x)
+    set_y = Service.action(rt.set_y)
+    set_theta = Service.action(rt.set_theta)
+    set_pos = Service.action(rt.set_pos)
+    mirror_pos = rt.mirror_pos
 
     def __init__(self):
         self.cs = CellaservProxy()
@@ -48,6 +57,10 @@ class Robot(Service):
         self.lidar.register_callback(self.lidar_callback)
 
         self.last_telemetry_received = 0
+
+        self.queue = Queue()
+        self.disabled = Event()
+
         super().__init__()
 
     @Service.event("match_color")
@@ -56,16 +69,46 @@ class Robot(Service):
             self.side = color == self.color1
 
     @Service.event("%s_telemetry" % ROBOT)
-    def update_position(self, telemetry, **kwargs):
+    def callback_telemetry(self, telemetry, **kwargs):
         tmp = time()
         print("[ROBOT] Time since last telemetry: " + str((tmp - self.last_telemetry_received) * 1000) + "s")
         self.last_telemetry_received = tmp
         self.lidar.set_position(Point(dict=telemetry), float(telemetry['theta']))
 
+    # Usefull
     def lidar_callback(self, cloud, shapes, robots):
         print('[ROBOT] Robots seen at: ')
         for robot in robots:
             print('-> ' + str(robot))
+
+    @Service.action
+    def enable(self):
+        self.disabled.clear()
+        Thread(target=self.run_queue).start()
+
+    @Service.action
+    def disable(self):
+        # Empty queue
+        while not self.queue.empty():
+            self.queue.get()
+
+
+        self.disabled.set()
+
+    def run_queue(self):
+        while not self.disabled.is_set():
+            task, args = self.queue.get()
+            self.publish('%s_robot_started' % ROBOT)
+            try:
+                status = task(*args)
+            except Exception as e:
+                print('[ROBOT Failed to execute task: %s' % str(e))
+
+            try:
+                status = RobotStatus(status)
+            except:
+                status RobotStatus.Unknow
+            self.publish('%s_robot_stopped' % ROBOT, status=status.value)
 
 if __name__ == '__main__':
     robot = Robot()
