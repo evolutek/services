53ff19a349e308320eddfbdff19e11d2640c1b1a
Speedy <yann-arthur.tcheumani@epitech.eu>
2021-06-05 20:14:34 +0200

    [ADD] callback function in queue

---
+++ python/evolutek/tests/interfaces/test_cellaserv_interface.py
@@ -0,0 +1,8 @@
+from evolutek.lib.cellaserv_interface import CellaservInterface
+
+def main():
+	interface = CellaservInterface("je me co youhouuuuu")
+	interface.loop()
+
+if __name__ == '__main__':
+	main()
