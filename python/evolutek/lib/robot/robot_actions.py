495bd0bce58ad13c0c3701e93f328d7ffcd876ca
Corentin Vigourt <cvigourt@scallog.com>
2021-06-08 14:56:12 +0200

    Update robot-actions/actuators

--- python/evolutek/lib/robot/robot_actions.py
+++ python/evolutek/lib/robot/robot_actions.py
@@ -1,5 +1,5 @@
 from evolutek.lib.status import RobotStatus
-from evolutek.lib.utils.wrappers import if_enabled
+from evolutek.lib.utils.wrappers import if_enabled, use_queue
 from time import sleep
 
 # TODO : check for abort and match end
@@ -9,32 +9,27 @@ from time import sleep
 #########
 
 @if_enabled
-def get_reef(self, use_queue=True):
+@use_queue
+def get_reef(self):
 
-    def f():
-        # TODO : Recal ?
+    # TODO : Recal ?
 
-        self.left_cup_holder.open(use_queue=False)
-        self.right_cup_holder.open(use_queue=False)
-        self.actuators.pumps_get([6, 7, 8, 9])
+    self.left_cup_holder.open(use_queue=False)
+    self.right_cup_holder.open(use_queue=False)
+    self.actuators.pumps_get([6, 7, 8, 9])
 
-        sleep(0.25)
+    sleep(0.25)
 
-        if self.move_trsl(300, 300, 300, 300, 0) != RobotStatus.Reached:
-            return RobotStatus.Failed
+    if self.move_trsl(300, 300, 300, 300, 0) != RobotStatus.Reached:
+        return RobotStatus.Failed
 
-        sleep(1)
+    sleep(1)
 
-        self.left_cup_holder_close(use_queue=False)
-        self.right_cup_holder_close(use_queue=False)
+    self.left_cup_holder_close(use_queue=False)
+    self.right_cup_holder_close(use_queue=False)
 
-        sleep(0.25)
-        if self.move_trsl(300, 300, 300, 300, 1)  != RobotStatus.Reached:
-            return RobotStatus.Failed
+    sleep(0.25)
+    if self.move_trsl(300, 300, 300, 300, 1)  != RobotStatus.Reached:
+        return RobotStatus.Failed
 
-        return RobotStatus.Done
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+    return RobotStatus.Done
--- python/evolutek/lib/robot/robot_actuators.py
+++ python/evolutek/lib/robot/robot_actuators.py
@@ -1,5 +1,5 @@
 from evolutek.lib.status import RobotStatus
-from evolutek.lib.utils.wrappers import if_enabled
+from evolutek.lib.utils.wrappers import if_enabled, use_queue
 
 #########
 # PUMPS #
@@ -19,27 +19,15 @@ def mirror_pump_id(self, id):
 
 # Raise Flags
 @if_enabled
-def flags_raise(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(6, 820))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def flags_raise(self):
+    return RobotStatus.get_status(self.actuators.ax_move(6, 820))
 
 # Close flags
 @if_enabled
-def flags_low(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(6, 512))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def flags_low(self):
+    return RobotStatus.get_status(self.actuators.ax_move(6, 512))
 
 #############
 # SIDE ARMS #
@@ -47,75 +35,39 @@ def flags_low(self, use_queue=True):
 
 # Left Arm Close
 @if_enabled
-def left_arm_close(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(2, 820))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def left_arm_close(self):
+    return RobotStatus.get_status(self.actuators.ax_move(2, 820))
 
 # Left Arm Open
 @if_enabled
-def left_arm_open(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(2, 512))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def left_arm_open(self):
+    return RobotStatus.get_status(self.actuators.ax_move(2, 512))
 
 # Left Arm Push Windsock
 @if_enabled
-def left_arm_push(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(2, 444))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def left_arm_push(self):
+    return RobotStatus.get_status(self.actuators.ax_move(2, 444))
 
 # Right Arm Close
 @if_enabled
-def right_arm_close(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(3, 204))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_arm_close(self):
+    return RobotStatus.get_status(self.actuators.ax_move(3, 204))
 
 # Right Arm Open
 @if_enabled
-def right_arm_open(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(3, 512))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_arm_open(self):
+    return RobotStatus.get_status(self.actuators.ax_move(3, 512))
 
 # Right Arm Push Windsock
 @if_enabled
-def right_arm_push(self, use_queue=True):
-
-    def f():
-        return RobotStatus.get_status(self.actuators.ax_move(3, 580))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_arm_push(self):
+    return RobotStatus.get_status(self.actuators.ax_move(3, 580))
 
 ###############
 # CUP HOLDERS #
@@ -123,74 +75,42 @@ def right_arm_push(self, use_queue=True):
 
 # Left CH Close
 @if_enabled
-def left_cup_holder_close(self, use_queue=True):
-
-    def f():
-        self.actuators.ax_set_speed(4, 256)
-        return RobotStatus.get_status(self.actuators.ax_move(4, 820))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def left_cup_holder_close(self):
+    self.actuators.ax_set_speed(4, 256)
+    return RobotStatus.get_status(self.actuators.ax_move(4, 820))
 
 # Left CH Open
 @if_enabled
-def left_cup_holder_open(self, use_queue=True):
-
-    def f():
-        self.actuators.ax_set_speed(4, 800)
-        return RobotStatus.get_status(self.actuators.ax_move(4, 512))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def left_cup_holder_open(self):
+    self.actuators.ax_set_speed(4, 800)
+    return RobotStatus.get_status(self.actuators.ax_move(4, 512))
 
 # Left CH Drop
 @if_enabled
-def left_cup_holder_drop(self, use_queue=True):
+@use_queue
+def left_cup_holder_drop(self):
     self.actuators.ax_set_speed(4, 800)
     return RobotStatus.get_status(self.actuators.ax_move(4, 490))
-    if status != RobotStatus.Done:
-        return RobotStatus.Failed
-    return RobotStatus.Done
 
 # Right CH Close
 @if_enabled
-def right_cup_holder_close(self, use_queue=True):
-
-    def f():
-        self.actuators.ax_set_speed(5, 256)
-        return RobotStatus.get_status(self.actuators.ax_move(5, 820))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_cup_holder_close(self):
+    self.actuators.ax_set_speed(5, 256)
+    return RobotStatus.get_status(self.actuators.ax_move(5, 820))
 
 # Right CH Open
 @if_enabled
-def right_cup_holder_open(self, use_queue=True):
-
-    def f():
-        self.actuators.ax_set_speed(5, 800)
-        return RobotStatus.get_status(self.actuators.ax_move(5, 512))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_cup_holder_open(self):
+    self.actuators.ax_set_speed(5, 800)
+    return RobotStatus.get_status(self.actuators.ax_move(5, 512))
 
 # Right CH Drop
 @if_enabled
-def right_cup_holder_drop(self, use_queue=True):
-
-    def f():
-        self.actuators.ax_set_speed(5, 800)
-        return RobotStatus.get_status(self.actuators.ax_move(5, 490))
-
-    if not use_queue:
-        return f()
-    else:
-        self.queue.run_action(f, [])
+@use_queue
+def right_cup_holder_drop(self):
+    self.actuators.ax_set_speed(5, 800)
+    return RobotStatus.get_status(self.actuators.ax_move(5, 490))
