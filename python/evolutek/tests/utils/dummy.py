3c8631184697efedeeefbe2acc739245c095844e
Corentin Vigourt <cvigourt@scallog.com>
2021-06-22 10:55:03 +0200

    Clean

--- python/evolutek/tests/utils/dummy.py
+++ python/evolutek/tests/utils/dummy.py
@@ -23,8 +23,7 @@ class Dummy(Service):
             self.publish('dummy_start', id=self.current_id)
             sleep(1)
             print('Say something')
-            d = RobotStatus.return_status(RobotStatus.Done, lol='XD', id=self.current_id)
-            self.publish('dummy_stop', **d)
+            self.publish('dummy_stop', **RobotStatus.return_status(RobotStatus.Done, lol='XD', id=self.current_id))
             self.current_id += 1
 
 dummy = Dummy()
