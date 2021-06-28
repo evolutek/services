b16ef7d0445f407b18bac00e12c6b0959fc6dcbf
PMI <evolutek.ionis@gmail.com>
2021-06-28 20:34:20 +0200

    Temporary fix (tm)

--- python/evolutek/lib/actuators/ax12.py
+++ python/evolutek/lib/actuators/ax12.py
@@ -2,6 +2,7 @@ import ctypes
 import os
 
 from evolutek.lib.component import Component, ComponentsHolder
+from functools import wraps
 
 LIBDXL_PATH = [".", "/usr/lib"]
 
@@ -25,54 +26,77 @@ AX_PRESENT_TEMPERATURE = 43
 AX_CW_ANGLE_LIMIT_L    = 6
 AX_CCW_ANGLE_LIMIT_L   = 8
 
+def five_times(function):
+    @wraps(function)
+    def wrapped(*args, **kwargs):
+        for _ in range(4):
+            function(*args, **kwargs)
+        return function(*args, **kwargs)
+    return wrapped
+
 class AX12(Component):
 
     def __init__(self, id):
         self.dxl = DXL
         super().__init__('AX12', id)
 
+    @five_times
     def dxl_get_result(self):
         return int(self.dxl.dxl_get_result())
 
+    @five_times
     def move(self, goal):
+        print(f"[{self.name}] Moving {self.id} to {goal}")
         return self.dxl.dxl_write_word(self.id, AX_GOAL_POSITION_L, int(goal))
 
+    @five_times
     def get_present_position(self):
         return self.dxl.dxl_read_word(self.id, AX_PRESENT_POSTION_L)
 
+    @five_times
     def get_present_speed(self):
         return self.dxl.dxl_read_word(self.id, AX_PRESENT_SPEED_L)
 
+    @five_times
     def get_present_load(self):
         return self.dxl.dxl_read_word(self.id, AX_PRESENT_LOAD_L)
 
+    @five_times
     def get_present_voltage(self):
         return self.dxl.dxl_read_byte(self.id, AX_PRESENT_VOLTAGE)
 
+    @five_times
     def get_present_temperature(self):
         return self.dxl.dxl_read_byte(self.id, AX_PRESENT_TEMPERATURE)
 
+    @five_times
     def get_cw_angle_limit(self):
         return self.dxl.dxl_read_word(self.id, AX_CW_ANGLE_LIMIT_L)
 
+    @five_times
     def get_ccw_angle_limit(self):
         return self.dxl.dxl_read_word(self.id, AX_CCW_ANGLE_LIMIT_L)
 
+    @five_times
     def mode_wheel(self):
         self.dxl.dxl_write_word(self.id, AX_CW_ANGLE_LIMIT_L, 0)
         return self.dxl.dxl_write_word(self.id, AX_CCW_ANGLE_LIMIT_L, 0)
 
+    @five_times
     def mode_joint(self):
         self.dxl.dxl_write_word(self.id, AX_CW_ANGLE_LIMIT_L, 0)
         return self.dxl.dxl_write_word(self.id, AX_CCW_ANGLE_LIMIT_L, 1023)
 
+    @five_times
     def moving_speed(self, speed):
         return self.dxl.dxl_write_word(self.id, AX_MOVING_SPEED_L, int(speed))
 
+    @five_times
     def turn(self, side, speed):
         self.dxl.dxl_write_word(self.id, AX_MOVING_SPEED_L,
                                 (2**10 if side else 0) | int(speed))
 
+    @five_times
     def free(self):
         self.dxl.dxl_write_byte(self.id, AX_TORQUE_ENABLE_B, 0)
 
