5a0e1b7153a0c4e2bd5e6a794ce1294cc348df08
PMI <evolutek.ionis@gmail.com>
2021-07-01 00:02:30 +0200

    fix service restart

--- python/evolutek/lib/service_reset.py
+++ python/evolutek/lib/service_reset.py
@@ -6,10 +6,11 @@ import RPi.GPIO as GPIO
 RESET_GPIO = 40
 
 def restartAll():
+    print("let's fucking go for a reset lmao")
     system("sudo systemctl restart trajman")
     system("sudo systemctl restart actuators")
-    system("sudo systemctl restart robot")
-    system("sudo systemctl restart ai")
+  #  system("sudo systemctl restart robot")
+    #system("sudo systemctl restart ai")
 
 def main():
     
@@ -17,14 +18,18 @@ def main():
     GPIO.setup(RESET_GPIO, GPIO.IN)
     cs = CellaservProxy()
     triggered = False
+    needRestart = False
+    lastVal = GPIO.HIGH
     while True:
         val = GPIO.input(RESET_GPIO)
-        if val == GPIO.LOW and not triggered:
-            triggered = True
-        elif val == GPIO.HIGH and triggered:
-            triggered = False
-        if triggered:
+        if val == GPIO.LOW and lastVal == GPIO.HIGH:
+            needRestart = True
+        elif val == GPIO.HIGH:
+            needRestart = False
+        if needRestart:
             restartAll()
+            needRestart = False
+        lastVal = val
 
         sleep(0.5)
 
