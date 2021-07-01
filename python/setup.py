74f237ff43f95c22a5d7ad18212b9545382d501c
PAL <evolutek.ionis@gmail.com>
2021-07-01 07:15:29 +0200

    Reset not restart

--- python/setup.py
+++ python/setup.py
@@ -31,7 +31,7 @@ setup(
             'trajman = evolutek.services.trajman:main',
             'match = evolutek.services.match:main',
             'robot = evolutek.services.robot:main',
-            'restart = evolutek.utils.restart:main',
+            'reset = evolutek.utils.reset:main',
         ],
     },
 )
