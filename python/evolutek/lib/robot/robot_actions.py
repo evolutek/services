722a72aea138974f96939e4130215c9408c4c154
Gabriel <gbrljorge@gmail.com>
2021-07-06 00:09:52 +0200

    Started translating drop_center to action

--- python/evolutek/lib/robot/robot_actions.py
+++ python/evolutek/lib/robot/robot_actions.py
@@ -117,7 +117,7 @@ def push_windsocks(self):
 
 @if_enabled
 @use_queue
-def drop_start_sorting(self):
+def drop_start(self):
 
     # Start position: 200 400 pi/2
 
@@ -314,6 +314,8 @@ def goto_anchorage(self):
 @use_queue
 def drop_center(self):
 
+    # Starting pos: 1400 1705 theta=0
+
     buoys_count = {
         Color.Green: 0,
         Color.Red: 0,
@@ -340,3 +342,90 @@ def drop_center(self):
         buoys_count[color] += 1
         score = calculate_score(buoys_count)
 
+    # Gets the first buoy in front of the zone
+    self.pumps_drop(ids='5', use_queue=False)
+    status = self.goto_avoid(x=1750, y=1705, use_queue=False)
+    if RobotStatus.get_status(status) != RobotStatus.Reached: return cleanup_and_exit(status)
+
+    # Recalibration on the central bar
+    status = self.goth(theta=pi/2, use_queue=False)
+    if RobotStatus.get_status(status) != RobotStatus.Reached: return cleanup_and_exit(status)
+    status = self.recalibration(x=False, x_sensor=RecalSensor.Right, decal_y=1511, use_queue=False)
+    if RobotStatus.get_status(status) != RobotStatus.Done: return cleanup_and_exit(status)
+
+    # Gets the second buoy in front of the zone
+
+"""
+robot.pumps_get(ids=[3])
+robot.goto(1720, 1855)
+input()
+
+robot.goto(1780, 1800)
+robot.goth(0)
+input()
+
+robot.pumps_get(ids=[2])
+robot.goto(1810, 1680)
+input()
+
+robot.goto(1780, 1800)
+robot.goth(0)
+input()
+
+robot.pumps_get(ids=[6])
+robot.goto(1810, 1910)
+input()
+
+robot.goto(1780, 1800)
+robot.goth(0)
+input()
+
+robot.goto(1810, 1800)
+input()
+
+trajman.move_trsl(dest=50, acc=1000, dec=1000, maxspeed=1000, sens=0)
+robot.pumps_drop(ids=[2, 3, 5, 6])
+robot.goto(1700, 1800)
+input()
+
+robot.front_arm_open()
+robot.goto(1720, 1800)
+input()
+
+robot.pumps_drop(ids=[1, 3, 4, 5])
+sleep(1)
+robot.goto(1500, 1800)
+input()
+
+robot.goth(pi)
+robot.left_cup_holder_drop()
+robot.right_cup_holder_drop()
+input()
+
+robot.goto(1620, 1800)
+input()
+
+robot.pumps_drop(ids=[7, 10])
+robot.goto(1580, 1800)
+input()
+
+robot.goth(3*pi/4)
+input()
+
+robot.pumps_drop(ids=[9])
+robot.left_cup_holder_close()
+robot.right_cup_holder_close()
+input()
+
+robot.goto(1540, 1800)
+robot.goth(-3*pi/4)
+robot.left_cup_holder_drop()
+input()
+
+robot.pumps_drop(ids=[8])
+robot.left_cup_holder_close()
+input()
+
+robot.goth(pi)
+input()
+"""
