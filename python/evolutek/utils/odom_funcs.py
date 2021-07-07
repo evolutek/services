907f85c09b610f6a40f7a3c073fdcbd5606d7bcf
PAL <evolutek.ionis@gmail.com>
2021-07-08 00:27:54 +0200

    PAL odomery

--- python/evolutek/utils/odom_funcs.py
+++ python/evolutek/utils/odom_funcs.py
@@ -1,14 +1,14 @@
 # This is degueulasse but don't delete it
 
 avg = 48.4
-robot = 'pmi'
+robot = 'pal'
 
 def gain(g):
     return avg*g, avg*(2-g)
 
 def cmd(g):
     g1,g2 = gain(g)
-    print(f'cellaservctl r trajman/{robot}.set_wheels_diameter w1={w1} w2={w2}')
+    print(f'cellaservctl r trajman/{robot}.set_wheels_diameter w1={g1} w2={g2}')
 
 def dist(dist):
     return 2000 - 120 - 55 - dist
