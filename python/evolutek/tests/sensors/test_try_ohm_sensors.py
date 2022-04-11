2cbd0ab19b259148058988c8c6826bbf1a548240
PMI <evolutek.ionis@gmail.com>
2022-04-12 01:02:51 +0200

    fix: test

--- python/evolutek/tests/sensors/test_try_ohm_sensors.py
+++ python/evolutek/tests/sensors/test_try_ohm_sensors.py
@@ -1,16 +1,16 @@
-from evolutek.lib.try_ohm_sensors import TryOhmSensors
+from evolutek.lib.sensors.try_ohm_sensors import TryOhmSensors
 from evolutek.lib.gpio.gpio_factory import GpioType, create_gpio
 
 from time import sleep
 
 sensors = {
     1 : [
-        create_gpio(13, 'sensor1a', dir=False, type=GpioType.MCP),
-        create_gpio(19, 'sensor1b', dir=False, type=GpioType.MCP)
+        create_gpio(13, 'sensor1a', dir=False, type=GpioType.RPI),
+        create_gpio(19, 'sensor1b', dir=False, type=GpioType.RPI)
     ],
     2 : [
-        create_gpio(16, 'sensor2a', dir=False, type=GpioType.MCP),
-        create_gpio(26, 'sensor2b', dir=False, type=GpioType.MCP)
+        create_gpio(16, 'sensor2a', dir=False, type=GpioType.RPI),
+        create_gpio(26, 'sensor2b', dir=False, type=GpioType.RPI)
     ]
 }
 
