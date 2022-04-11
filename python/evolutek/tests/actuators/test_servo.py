24ea00ae43bcff28951bd59ca690991d8868f879
PMI <evolutek.ionis@gmail.com>
2022-04-11 23:55:19 +0200

    add: test for new ServoController

--- python/evolutek/tests/actuators/test_servo.py
+++ python/evolutek/tests/actuators/test_servo.py
@@ -4,16 +4,29 @@ from time import sleep
 
 pumps = {
     0: [
-            33,
+            3333,
             180
+    ],
+    11: [
+        50,
+        180,
+    ],
+    15: [
+        333,
+        180
     ]
+
 }
 
 
 servo_controller = ServoHandler(pumps, 1)
 print(servo_controller)
 
-for servo in servo_controller:
+"""for servo in servo_controller:
     servo_controller[servo].set_angle(180)
     sleep(1)
     servo_controller[servo].set_angle(0)
+    """
+servo_controller.set_angle_all({0: 180, 11:180, 15:180})
+sleep(1)
+servo_controller.set_angle_all({0:0, 11:0, 15:0})
