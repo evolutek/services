a6a17a916e495ebe6f5349a3978429b9720ff664
PAL <evolutek.ionis@gmail.com>
2022-05-24 14:58:08 +0200

    Fix: score in drop_start didn't add up

--- python/evolutek/lib/robot/actions/drop_start.py
+++ python/evolutek/lib/robot/actions/drop_start.py
@@ -7,7 +7,7 @@ def drop_start(self):
     self.goto_avoid(x = 380, y = 225, async_task = False)
     self.goth(theta = 0, async_task = False)
     score = 1 if get_boolean(self.actuators.proximity_sensor_read(id = 1)) else 0
-    score = 1 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0
+    score += 1 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0
     self.pumps_drop(ids="1,3", async_task=False)
 
     return RobotStatus.return_status(RobotStatus.Done, score=score)
