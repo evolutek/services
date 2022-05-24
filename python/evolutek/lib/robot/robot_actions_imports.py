c18a451e980e1c474373a2c1dde08c29e10f86cf
PAL <evolutek.ionis@gmail.com>
2022-05-24 08:51:52 +0200

    Missing import elevatorspeed

--- python/evolutek/lib/robot/robot_actions_imports.py
+++ python/evolutek/lib/robot/robot_actions_imports.py
@@ -4,7 +4,7 @@ from evolutek.lib.status import RobotStatus
 from evolutek.lib.utils.color import Color
 from evolutek.lib.utils.task import async_task
 from evolutek.lib.utils.wrappers import if_enabled
-from evolutek.lib.robot.robot_actuators import ElevatorConfig, HeadConfig, FrontArmsEnum, HeadSpeed
+from evolutek.lib.robot.robot_actuators import ElevatorConfig, HeadConfig, FrontArmsEnum, HeadSpeed, ElevatorSpeed
 
 from time import sleep
 from math import pi
