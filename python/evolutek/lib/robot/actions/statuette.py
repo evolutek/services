04e4debd35dfdb8fe506a1e385c74d1990de9804
PAL <evolutek.ionis@gmail.com>
2022-05-24 16:46:42 +0200

    fix drop statuette arm not reset

--- python/evolutek/lib/robot/actions/statuette.py
+++ python/evolutek/lib/robot/actions/statuette.py
@@ -7,7 +7,7 @@ def statuette(self):
     def cleanup():
         print("\n\n\n\n\nCLEANUP\n\n\n\n\n")
         self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Closed, async_task=False)
-        self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.StoreStatuette, async_task=False)    
+        self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.StoreStatuette, async_task=False)
     has_dropped = pickup_statuette(self)
     score = 0
     if (has_dropped):
@@ -31,6 +31,6 @@ def statuette(self):
         cleanup()
         return RobotStatus.return_status(RobotStatus.get_status(status))
     sleep(0.3)
-    self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Mid, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Closed, async_task=False)
     self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.StoreStatuette, async_task=False)
     return RobotStatus.return_status(RobotStatus.Done, score=score)
