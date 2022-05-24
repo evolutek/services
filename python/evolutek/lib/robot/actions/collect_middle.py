457249b493036fde88d154b6951f2e6839d038ad
PAL <evolutek.ionis@gmail.com>
2022-05-24 12:06:16 +0200

    Fix: collect_middle

--- python/evolutek/lib/robot/actions/collect_middle.py
+++ python/evolutek/lib/robot/actions/collect_middle.py
@@ -18,7 +18,7 @@ def collect_middle(self):
     self.goto_avoid(675,600, async_task=False, timeout=10)
     self.goth(theta=pi/2, async_task=False)
     self.snowplow_open(async_task=False)
-    self.goto_avoid(675,850, async_task=False, timeout=10)
+    self.goto_avoid(675,900, async_task=False, timeout=10)
     sleep(0.2)
         #set elevator to down
     self.set_elevator_config(arm=1, config=ElevatorConfig.Down, async_task=False)
@@ -39,6 +39,10 @@ def collect_middle(self):
     self.set_head_config(arm=3, config=HeadConfig.Galery, async_task=False)
 
     #drop palets to galery
+        #check score
+    score = (1 if get_boolean(self.actuators.proximity_sensor_read(id = 1)) else 0) * flip * 3
+    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 2)) else 0) * flip * 3
+    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0) * flip * 3
         #go to galery
     self.goto_avoid(320, 810, async_task=False, timeout=10)
     self.snowplow_close(async_task=False)
@@ -46,9 +50,6 @@ def collect_middle(self):
     self.goth(theta=pi, async_task=False)
     self.goto_avoid(190, 810, async_task=False, timeout=10)
         #score
-    score = (1 if get_boolean(self.actuators.proximity_sensor_read(id = 1)) else 0) * flip * 3
-    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 2)) else 0) * flip * 3
-    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0) * flip * 3
     self.pumps_drop(ids="1,2,3", async_task=False)
         #go back
     self.goto_avoid(320, 810, async_task=False, timeout=10)
