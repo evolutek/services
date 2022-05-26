122fe2f9c46cabfa22bd16c187b9539d9e919f3e
Célian A <celian.aldehuelo-mateos@epita.fr>
2022-05-26 17:30:44 +0200

    Change on place_under action

--- python/evolutek/tests/actions/place_under.py
+++ python/evolutek/tests/actions/place_under.py
@@ -4,9 +4,6 @@ ROBOT="pal"
 
 def home():
     cs = CellaservProxy()
-    #cs.robot[ROBOT].recalibration(x=False, y=True, x_sensor="right", init=True)
-    #cs.robot[ROBOT].goto(1830, 1130)
-    
     cs.robot[ROBOT].place_under()
 
 home()
