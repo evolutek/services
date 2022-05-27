1b715f6cfa9553adc9262c661dd81b9cf9236ec7
PAL <evolutek.ionis@gmail.com>
2022-05-27 22:39:05 +0200

    Upgrade drops

--- python/evolutek/lib/robot/actions/drop.py
+++ python/evolutek/lib/robot/actions/drop.py
@@ -25,6 +25,7 @@ def drop_until(self, amount = 1, drop_level = 0):
     sleep(0.5)
     status.append(self.clamp_open(async_task=False))
     sleep(0.2)
+    score = min(amount, len(self.cakes_stack))
     self.cakes_stack = self.cakes_stack[amount:]
     if len(self.cakes_stack) > 0:
         status.append(self.elevator_move(GRAB_LEVELS[amount - 1 + drop_level], async_task=False))
@@ -34,26 +35,18 @@ def drop_until(self, amount = 1, drop_level = 0):
     status.append(self.elevator_move("High", async_task=False))
     sleep(0.5)
     status.append(self.forward(-125, async_task=False))
-    return RobotStatus.check(*status, score=3)
+    return RobotStatus.check(*status, score=score)
 
 
 @if_enabled
 @async_task
 def drop_stacks(self, n=-1):
-    status = []
     score = 0
     i = 0
-    while i != n: # and len(self.cakes_stack) > 0:
-        status = []
-        status.append(self.drop_until(amount=3, async_task=False))
-        #status.append(self.elevator_move("Low", async_task=False))
-        #status.append(self.clamp_open(async_task=False))
-        #status.append(self.elevator_move("GetFourth", async_task=False))
-        #status.append(self.clamp_close(async_task=False))
-        #status.append(self.forward(-125, async_task=False))
-        #self.cakes_stack = self.cakes_stack[3:]
-        if RobotStatus.get_status(RobotStatus.check(*status)) == RobotStatus.Done:
-            score += 3 #min(3, len(self.cakes_stack))
+    while i != n and len(self.cakes_stack) > 0:
+        r = self.drop_until(amount=3, async_task=False)
+        if RobotStatus.get_status(r) == RobotStatus.Done:
+            score += r['score']
         else:
             return RobotStatus.return_status(RobotStatus.Failed, score=score)
         i += 1
