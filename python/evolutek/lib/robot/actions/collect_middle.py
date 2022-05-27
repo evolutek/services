3e29233fedd7b99f617b40f999f48a4dd48d9ec4
Célian A <celian.aldehuelo-mateos@epita.fr>
2022-05-27 17:48:47 +0200

    Check status in collect_middle action

--- python/evolutek/lib/robot/actions/collect_middle.py
+++ python/evolutek/lib/robot/actions/collect_middle.py
@@ -1,71 +1,93 @@
 from evolutek.lib.robot.robot_actions_imports import *
 from evolutek.lib.utils.boolean import get_boolean
 
+
+def cleanup(self):
+    self.set_elevator_config(arm=FrontArmsEnum.Left,   config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Right,  config=ElevatorConfig.Mid, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Left,   config=HeadConfig.Closed, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Closed, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Right,  config=HeadConfig.Closed, async_task=False)
+    sleep(0.5)
+
+
 @if_enabled
 @async_task
 def collect_middle(self):
+    score = 0
     flip = 1 #put 2 if flip else 1
-    #grab palets
-    self.set_elevator_config(arm=1, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=2, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=3, config=ElevatorConfig.Mid, async_task=False)
-    self.set_head_config(arm=1, config=HeadConfig.Down, async_task=False)
-    self.set_head_config(arm=2, config=HeadConfig.Down, async_task=False)
-    self.set_head_config(arm=3, config=HeadConfig.Down, async_task=False)
 
-        #move to palets with snowplow
-    self.goth(theta=pi/2, async_task=False)
-    self.goto_avoid(675,640, async_task=False, timeout=10)
-    self.goth(theta=pi/2, async_task=False)
-    self.snowplow_open(async_task=False)
-    speed = self.trajman.get_speeds()['trmax']
-    self.trajman.set_trsl_max_speed(80)
-    self.goto_avoid(675,820, async_task=False, timeout=10)
-    self.trajman.set_trsl_max_speed(speed)
-    sleep(0.2)
+    # Grab palets
+    self.set_elevator_config(arm=FrontArmsEnum.Left,   config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Right,  config=ElevatorConfig.Mid, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Left,   config=HeadConfig.Down, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Down, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Right,  config=HeadConfig.Down, async_task=False)
 
-        #set elevator to down
-    self.set_elevator_config(arm=1, config=ElevatorConfig.Down, async_task=False)
-    self.set_elevator_config(arm=2, config=ElevatorConfig.Down, async_task=False)
-    self.set_elevator_config(arm=3, config=ElevatorConfig.Down, async_task=False)
+    # Move to palets with snowplow
+    status = RobotStatus.get_status(self.goth(theta=pi/2, async_task=False))
+    if status == RobotStatus.Reached:
+        status = RobotStatus.get_status(self.goto_avoid(675 ,640, async_task=False, timeout=10))
+        if status == RobotStatus.Reached:
+            status = RobotStatus.get_status(self.goth(theta=pi/2, async_task=False))
+            if status == RobotStatus.Reached:
+                self.snowplow_open(async_task=False)
+                speed = self.trajman.get_speeds()['trmax']
+                self.trajman.set_trsl_max_speed(80)
+                status = RobotStatus.get_status(self.goto_avoid(675,820, async_task=False, timeout=10))
+                self.trajman.set_trsl_max_speed(speed)
+                sleep(0.2)
+    
+    if status != RobotStatus.Reached:
+        cleanup(self)
+        return RobotStatus.return_status(status, score=score)
+
+    # Set elevator to down
+    self.set_elevator_config(arm=FrontArmsEnum.Left,   config=ElevatorConfig.Down, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.Down, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Right,  config=ElevatorConfig.Down, async_task=False)
     sleep(0.2)
     
-        #activated pump
-    self.pumps_get(ids="1", async_task=False)
-    self.pumps_get(ids="2", async_task=False)
-    self.pumps_get(ids="3", async_task=False)
+    # Activated pump
+    self.pumps_get(ids="1,2,3", async_task=False)
     sleep(1)
 
-    self.set_elevator_config(arm=1, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=2, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=3, config=ElevatorConfig.Mid, async_task=False)
-    self.set_head_config(arm=1, config=HeadConfig.Galery, async_task=False)
-    self.set_head_config(arm=2, config=HeadConfig.Galery, async_task=False)
-    self.set_head_config(arm=3, config=HeadConfig.Galery, async_task=False)
+    # Set arms in the correct position for placing
+    self.set_elevator_config(arm=FrontArmsEnum.Left,   config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Center, config=ElevatorConfig.Mid, async_task=False)
+    self.set_elevator_config(arm=FrontArmsEnum.Right,  config=ElevatorConfig.Mid, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Left,   config=HeadConfig.Galery, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Center, config=HeadConfig.Galery, async_task=False)
+    self.set_head_config(arm=FrontArmsEnum.Right,  config=HeadConfig.Galery, async_task=False)
 
-    #drop palets to galery
-        #check score
-    score = (1 if get_boolean(self.actuators.proximity_sensor_read(id = 1)) else 0) * flip * 3
-    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 2)) else 0) * flip * 3
-    score += (1 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0) * flip * 3
+    # Check score
+    score += (3 if get_boolean(self.actuators.proximity_sensor_read(id = 1)) else 0) * flip
+    score += (3 if get_boolean(self.actuators.proximity_sensor_read(id = 2)) else 0) * flip
+    score += (3 if get_boolean(self.actuators.proximity_sensor_read(id = 3)) else 0) * flip
     
-        #go to galery
+    # Close snowplow
     self.snowplow_close(async_task=False)
+
+    # Go to galery
     self.goth(pi, async_task=False)
-
     self.goto_avoid(190, 810, async_task=False, timeout=10)
-        #score
-    self.pumps_drop(ids="1,2,3", async_task=False)
-        #go back
-    self.goto_avoid(320, 810, async_task=False, timeout=10)
+    if status != RobotStatus.Reached:
+        cleanup(self)
+        return RobotStatus.return_status(status, score=score)
 
-        #reset arms
-    self.set_elevator_config(arm=1, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=2, config=ElevatorConfig.Mid, async_task=False)
-    self.set_elevator_config(arm=3, config=ElevatorConfig.Mid, async_task=False)
-    self.set_head_config(arm=1, config=HeadConfig.Closed, async_task=False)
-    self.set_head_config(arm=2, config=HeadConfig.Closed, async_task=False)
-    self.set_head_config(arm=3, config=HeadConfig.Closed, async_task=False)
+    # Drop palets to galery
+    self.pumps_drop(ids="1,2,3", async_task=False)
+
+    # Go back
+    self.goto_avoid(320, 810, async_task=False, timeout=10)
+    if status != RobotStatus.Reached:
+        cleanup(self)
+        return RobotStatus.return_status(status, score=score)
+
+    # Cleanup
+    cleanup(self)
 
     return RobotStatus.return_status(RobotStatus.Done,score=score)
 
