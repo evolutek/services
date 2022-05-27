3f31e77764059b31dd28e694f65e22833304b3a2
PAL <evolutek.ionis@gmail.com>
2022-05-27 02:46:26 +0200

    Working strategy for PMI

--- python/evolutek/lib/robot/actions/disguise.py
+++ python/evolutek/lib/robot/actions/disguise.py
@@ -3,5 +3,6 @@ from evolutek.lib.robot.robot_actions_imports import *
 @if_enabled
 @async_task
 def disguise(self, enable=True):
-    if enable: return RobotStatus.check(self.disguise_on(), score=5)
-    return RobotStatus.check(self.disguise_off())
+    #if enable: return RobotStatus.check(self.disguise_on(), score=5)
+    #return RobotStatus.check(self.disguise_off())
+    return RobotStatus.return_status(RobotStatus.Done) # TODO
