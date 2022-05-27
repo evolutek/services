1e8aa0c195dd76ca66c34a0e7c2971cba5faf600
PAL <evolutek.ionis@gmail.com>
2022-05-27 12:29:01 +0200

    Update suck rack

--- python/evolutek/lib/robot/actions/suck_rack.py
+++ python/evolutek/lib/robot/actions/suck_rack.py
@@ -12,15 +12,15 @@ class Rack:
 RACKS = [
     Rack(True,  'y', 1500), # Rack position: (1500, 0)
     Rack(False, 'y', 1500), # Rack position: (1500, 2000)
-    Rack(True,  'x', 1000), # Rack position: (0, 1000)
-    Rack(False, 'x', 1000), # Rack position: (3000, 1000)
+    Rack(False, 'x', 1000), # Rack position: (0, 1000)
+    Rack(True,  'x', 1000), # Rack position: (3000, 1000)
 ]
 
 
 @if_enabled
 @async_task
 def suck_rack(self, rack, recal_sensor, quantity=7):
-    OFFSET = 120 # Distance between robot center and vacuum
+    OFFSET = 182.5 # Distance between robot center and vacuum
 
     rack = RACKS[int(rack)]
     quantity = int(quantity)
@@ -31,12 +31,10 @@ def suck_rack(self, rack, recal_sensor, quantity=7):
         init=True,
         x_sensor=(recal_sensor if rack.axe != 'x' else "no"),
         y_sensor=(recal_sensor if rack.axe == 'x' else "no"),
-        side_x = rack.side, side_y = rack.side
+        side_x = rack.side, side_y = rack.side, async_task=False
     )
-    sleep(5)
 
     status = []
-    print("recalibration done")
 
     forward_value = 30
 
@@ -45,13 +43,13 @@ def suck_rack(self, rack, recal_sensor, quantity=7):
     if rack.axe == 'x':
         GAP = 200
         LENGTH = 30*10-15 + GAP
-        x = LENGTH if rack.side else 3000 - LENGTH
+        x = LENGTH if not rack.side else 3000 - LENGTH
         self.goto_avoid(x=x, y=position['y'], async_task=False, mirror=False)
         y = rack.pos - OFFSET if position['y'] < 1000 else rack.pos + OFFSET
         self.goto_avoid(x=x, y=y, async_task=False, mirror=False)
         self.goth(pi if rack.side else 0, async_task=False, mirror=False)
-        self.goto_avoid(x=x-GAP if rack.side else x+GAP, y=y, async_task=False, mirror=False)
-        left_vacuum = not rack.side
+        self.goto_avoid(x=x-GAP if not rack.side else x+GAP, y=y, async_task=False, mirror=False)
+        left_vacuum = rack.side
     else:
         y = OFFSET if rack.side else 2000 - OFFSET
         self.goto_avoid(x=position['x'], y=y, async_task=False, mirror=False)
@@ -64,6 +62,9 @@ def suck_rack(self, rack, recal_sensor, quantity=7):
         status.append(self.extend_left_vacuum(async_task=False))
     else:
         status.append(self.extend_right_vacuum(async_task=False))
+
+    self.push_isol(async_task=False)
+    sleep(0.5)
 
     for _ in range(quantity):
         status.append(self.turbine_on(async_task=False))
