d2d6588ee6ec28e517e790c5761a7f73812896ae
reza0310 <michelageou@gmail.com>
2023-03-25 18:27:47 +0100

    Added "grab_more" function in actuators

--- python/evolutek/lib/robot/robot_actuators.py
+++ python/evolutek/lib/robot/robot_actuators.py
@@ -4,30 +4,44 @@ from evolutek.lib.utils.color import Color
 from evolutek.lib.utils.task import async_task
 from evolutek.lib.utils.wrappers import if_enabled
 
+def check_status(*args):
+    for stat in args:
+        if stat != RobotStatus.Done:
+            return RobotStatus.return_status(RobotStatus.Failed)
+    return RobotStatus.return_status(RobotStatus.Done)
+
 @if_enabled
 @async_task
 def clamp_open(self):
     status1 = RobotStatus.get_status(self.actuators.servo_set_angle(0, 0))
     status2 = RobotStatus.get_status(self.actuators.servo_set_angle(15, 180))
-    return RobotStatus.return_status(RobotStatus.Done if status1 == RobotStatus.Done and status2 == RobotStatus.Done else RobotStatus.Failed)
+    return check_status(status1, status2)
 
 @if_enabled
 @async_task
 def clamp_close(self):
     status1 = RobotStatus.get_status(self.actuators.servo_set_angle(0, 25))
     status2 = RobotStatus.get_status(self.actuators.servo_set_angle(15, 155))
-    return RobotStatus.return_status(RobotStatus.Done if status1 == RobotStatus.Done and status2 == RobotStatus.Done else RobotStatus.Failed)
+    return check_status(status1, status2)
 
 @if_enabled
 @async_task
 def elevator_up(self):
     status1 = RobotStatus.get_status(self.actuators.ax_move(1, 512))
     status2 = RobotStatus.get_status(self.actuators.ax_move(2, 512))
-    return RobotStatus.return_status(RobotStatus.Done if status1 == RobotStatus.Done and status2 == RobotStatus.Done else RobotStatus.Failed)
+    return check_status(status1, status2)
 
 @if_enabled
 @async_task
 def elevator_down(self):
     status1 = RobotStatus.get_status(self.actuators.ax_move(1, 775))
     status2 = RobotStatus.get_status(self.actuators.ax_move(2, 180))
-    return RobotStatus.return_status(RobotStatus.Done if status1 == RobotStatus.Done and status2 == RobotStatus.Done else RobotStatus.Failed)
+    return check_status(status1, status2)
+
+@if_enabled
+@async_task
+def grab_more(self):
+    status1 = RobotStatus.get_status(clamp_open())
+    status2 = RobotStatus.get_status(elevator_down())
+    status3 = RobotStatus.get_status(clamp_close())
+    return check_status(status1, status2, status3)
