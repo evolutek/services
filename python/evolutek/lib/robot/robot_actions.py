084c16c8059351e8cc3c17e2f63517ec080af6e5
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2023-05-14 18:05:00 +0200

    [FIX] minor import fix

--- python/evolutek/lib/robot/robot_actions.py
+++ python/evolutek/lib/robot/robot_actions.py
@@ -1,5 +1,5 @@
 from evolutek.lib.robot.actions.goto_random import goto_random
-from evolutek.lib.robot.actions.go_grab import roam_stacks, roam_zones, go_grab_one_stack, go_drop_all
+from evolutek.lib.robot.actions.go_grab import roam_stacks, roam_zones, go_grab_one_stack, go_drop_all, stack_and_grab
 from evolutek.lib.robot.actions.empty_cherries import empty_n_cherries, empty_all_cherries
 from evolutek.lib.robot.actions.fill_cherries import fill_n_cherries, fill_all_cherries, set_cherry_count
 from evolutek.lib.robot.actions.vaccum_cherry import vacuum_10_cherry_right, vacuum_10_cherry_left 
