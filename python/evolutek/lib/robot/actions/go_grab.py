e7ed97303e83287c4d472d3a67316efc7e2b21d1
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2023-05-15 00:01:29 +0200

    distance fix

--- python/evolutek/lib/robot/actions/go_grab.py
+++ python/evolutek/lib/robot/actions/go_grab.py
@@ -231,7 +231,7 @@ def grab_first_two_stacks(self, first_id = 1, first_color_name = "Pink"):
     print(status)
     sleep(0.5)
 
-    go_to_point = robot_pos.compute_offset_point(f_stack_pos, 40)
+    go_to_point = robot_pos.compute_offset_point(f_stack_pos, 50)
     status = self.goto_avoid(x=go_to_point.x, y=go_to_point.y, mirror=False, async_task=False, timeout=10)
     if RobotStatus.get_status(status) != RobotStatus.Reached:
         return RobotStatus.return_status(RobotStatus.Failed)
