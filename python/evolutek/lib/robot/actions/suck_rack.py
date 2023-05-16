c3e85abf70c679d7589b9f9727fda1ec48c76bda
reza0310 <michelageou@gmail.com>
2023-05-16 18:20:03 +0200

    Suck rack action

--- python/evolutek/lib/robot/actions/suck_rack.py
+++ python/evolutek/lib/robot/actions/suck_rack.py
@@ -3,82 +3,28 @@ from evolutek.lib.robot.robot_actions_imports import *
 
 @if_enabled
 @async_task
-def suck_rack(self):
-    return check_status()
-
-'''def vacuum_10_cherry_left(self):
-
-    # Set to drop to stock cherry
-    if RobotStatus.get_status(self.push_drop(async_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
-    # Open Left arm
-    if RobotStatus.get_status(self.extend_left_vacuum(asybc_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
+def suck_rack(self, left=True, forward=True):
+    status = []
     init_pos = self.trajman.get_position()
-    theta = float(init_pos["theta"])
-    pos_to_go = Point(dict=init_pos)
+    quantity = 7 if 500 < init_pos["y"] < 1500 else 10
+    forward_value = 30 if forward else -30
 
-    for i in range(10):
-        # Suck one cherry
-        if RobotStatus.get_status(self.turbine_on(async_task=False)) != RobotStatus.Done:
-            return RobotStatus.return_status(RobotStatus.Failed)
-        sleep(1)
-        
-        if RobotStatus.get_status(self.turbine_off(async_task=False)) != RobotStatus.Done:
-            return RobotStatus.return_status(RobotStatus.Failed)
-        sleep(2)
+    if left:
+        self.extend_left_vacuum()
+    else:
+        self.extend_right_vacuum()
 
+    for i in range(quantity):
+        self.turbine_on()
+        sleep(0.5)
+        self.turbine_off()
+        self.forward(forward_value)
         self.cherry_count += 1
-
-        # Move 30 mm forward
-        pos_to_go = current_pos.compute_delta(float(theta), 30)
-        status_going = self.goto_avoid(x=pos_to_go.x, y=pos_to_go.y, async_task=False, timeout=10)
-        if RobotStatus.get_status(status_going) != RobotStatus.Reached:
-            return RobotStatus.return_status(RobotStatus.get_status(status_going))
-
-    # Close
-    if RobotStatus.get_status(self.retract_left_vacuum(async_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
-    return RobotStatus.return_status(RobotStatus.Done, score=0)
-
-
-def vacuum_10_cherry_right(self):
-
-    # Set to drop to stock cherry
-    if RobotStatus.get_status(self.push_drop(async_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
-    # Open Left arm
-    if RobotStatus.get_status(self.extend_right_vacuum(asybc_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
-    init_pos = self.trajman.get_position()
-    theta = float(init_pos["theta"])
-    pos_to_go = Point(dict=init_pos)
-
-    for i in range(10):
-        # Suck one cherry
-        if RobotStatus.get_status(self.turbine_on(async_task=False)) != RobotStatus.Done:
-            return RobotStatus.return_status(RobotStatus.Failed)
         sleep(1)
-        
-        if RobotStatus.get_status(self.turbine_off(async_task=False)) != RobotStatus.Done:
-            return RobotStatus.return_status(RobotStatus.Failed)
-        sleep(2)
 
-        self.cherry_count += 1
+    if left:
+        self.retract_left_vacuum()
+    else:
+        self.retract_right_vacuum()
 
-        # Move 30 mm forward
-        pos_to_go = current_pos.compute_delta(float(theta), 30)
-        status_going = self.goto_avoid(x=pos_to_go.x, y=pos_to_go.y, async_task=False, timeout=10)
-        if RobotStatus.get_status(status_going) != RobotStatus.Reached:
-            return RobotStatus.return_status(RobotStatus.get_status(status_going))
-
-    # Close
-    if RobotStatus.get_status(self.retract_right_vacuum(async_task=False)) != RobotStatus.Done:
-        return RobotStatus.return_status(RobotStatus.Failed)
-
-    return RobotStatus.return_status(RobotStatus.Done, score=0)'''
+    return check_status(*status, score=0)
