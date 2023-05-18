61f9f7806aec03b13c37fb3584f9ba809b91b8eb
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2023-05-18 22:21:17 +0200

    added distance check blahblahblahb

--- python/evolutek/lib/robot/actions/stack_and_grab.py
+++ python/evolutek/lib/robot/actions/stack_and_grab.py
@@ -81,11 +81,15 @@ def stack_and_grab(self, id = 1, color_name = "Pink"):
     status.append(self.goth(robot_pos.compute_angle(stack_pos), async_task=False, mirror=False))
 
     if (len(self.cakes_stack) > 0):
-        go_to_point = robot_pos.compute_offset_point(stack_pos, -80)
+        go_to_point = robot_pos.compute_offset_point(stack_pos, -90)
         status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
         print(f"Stack is in front ? : {self.actuators.proximity_sensor_read(id=1)}")
         if (not self.actuators.proximity_sensor_read(id=1)):
             return RobotStatus.check(*status)
+
+        go_to_point = robot_pos.compute_offset_point(stack_pos, -80)
+        status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
+
         status.append(self.clamp_open(async_task=False))
         sleep(0.5)
 
