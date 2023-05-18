9dd149314d8537b739a54119884faa64d3853578
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2023-05-19 00:09:16 +0200

    please make it stop

--- python/evolutek/lib/robot/actions/stack_and_grab.py
+++ python/evolutek/lib/robot/actions/stack_and_grab.py
@@ -111,15 +111,23 @@ def stack_and_grab(self, id = 1, color_name = "Pink"):
             go_to_point = robot_pos.compute_offset_point(stack_pos, -110)
             status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
             sleep(0.3)
-
-        go_to_point = robot_pos.compute_offset_point(stack_pos, -80)
-        status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
-        print(f"Stack is in front ? : {self.actuators.proximity_sensor_read(id=1)}")
-        if (not self.actuators.proximity_sensor_read(id=1)):
-            return RobotStatus.check(*status)
-        sleep(0.5)
-        status.append(self.clamp_close(async_task=False))
-        sleep(0.5)
+            print(f"Stack is in front ? : {self.actuators.proximity_sensor_read(id=1)}")
+            if (not self.actuators.proximity_sensor_read(id=1)):
+                return RobotStatus.check(*status)
+            go_to_point = robot_pos.compute_offset_point(stack_pos, -80)
+            status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
+            sleep(0.5)
+            status.append(self.clamp_close(async_task=False))
+            sleep(0.5)
+        else:
+            go_to_point = robot_pos.compute_offset_point(stack_pos, -80)
+            status.append(self.goto_avoid(x=go_to_point.x, y=go_to_point.y, async_task=False, mirror=False))
+            print(f"Stack is in front ? : {self.actuators.proximity_sensor_read(id=1)}")
+            if (not self.actuators.proximity_sensor_read(id=1)):
+                return RobotStatus.check(*status)
+            sleep(0.5)
+            status.append(self.clamp_close(async_task=False))
+            sleep(0.5)
 
     for _ in range(3):
         self.cakes_stack.append(color_name)
