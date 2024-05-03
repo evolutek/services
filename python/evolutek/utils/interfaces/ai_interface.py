4200c4506bc56564658c0534900dd8afeece01e6
Kolte200 <celian.aldehuelo@gmail.com>
2024-05-03 11:58:41 +0200

    Update interface to restart match on button press (and clean a little the code)

--- python/evolutek/utils/interfaces/ai_interface.py
+++ python/evolutek/utils/interfaces/ai_interface.py
@@ -17,31 +17,29 @@ FONT_SMALL = None
 
 
 class IFrame(tk.Frame):
-	def __init__(self, container, border=0, **kwargs):
-		self.parent = container
-		self.cs = container.cs
-		super().__init__(self.parent.window, bd=border, **kwargs)
+	def __init__(self, root, parent, border=0, **kwargs):
+		super().__init__(parent, bd=border, **kwargs)
+		self.root = root
 
 	def update_interface(self):
 		pass
 
-	def __init_interface(self):
+	def init_interface(self):
 		pass
 
 
 class StrategyFrame(IFrame):
-	def __init__(self, container):
+	def __init__(self, root, parent):
+		super().__init__(root, parent)
 		self.strategy_number = tk.IntVar()
-		super().__init__(container)
-		self.__init_interface()
+		self.init_interface()
 
 	def action_strategy(self):
-		self.cs.ai[ROBOT].set_strategy(self.strategy_number.get())
-		print(f"New strategy: {self.cs.ai[ROBOT].get_strategies().keys(self.strategy_number.get())}")
+		self.root.cs.ai[ROBOT].set_strategy(self.strategy_number.get())
+		print(f"New strategy: {self.root.cs.ai[ROBOT].get_strategies().keys(self.strategy_number.get())}")
 
-	def __create_radio_strategy(self):
-		#list_strategy = {"Strategie 1": 0, "Strategie 2": 1}
-		list_strategy = self.cs.ai[ROBOT].get_strategies()
+	def init_interface(self):
+		list_strategy = self.root.cs.ai[ROBOT].get_strategies()
 		buttons = []
 		for strategy in list_strategy:
 			l = tk.Radiobutton(
@@ -55,17 +53,11 @@ class StrategyFrame(IFrame):
 			l.pack(side=tk.TOP, fill=tk.X)
 			buttons.append(l)
 
-	def __init_interface(self):
-		self.__create_radio_strategy()
-
-	def update_interface(self):
-		pass
-
 
 class ButtonSystem(IFrame):
-	def __init__(self, container):
-		super().__init__(container)
-		self.__init_interface()
+	def __init__(self, root, parent):
+		super().__init__(root, parent)
+		self.init_interface()
 
 	@staticmethod
 	def shutdown():
@@ -76,7 +68,7 @@ class ButtonSystem(IFrame):
 		os.system("sudo reboot")
 
 	def close(self):
-		self.parent.close()
+		self.root.close()
 
 	def create_buttons(self):
 		self.grid_columnconfigure(0, weight=1, uniform="c")
@@ -86,99 +78,102 @@ class ButtonSystem(IFrame):
 		tk.Button(self, text="Shutdown", command=self.shutdown, font=FONT_MEDIUM).grid(row=0, column=1, sticky=tk.N)
 		tk.Button(self, text="Close", command=self.close, font=FONT_MEDIUM).grid(row=0, column=2, sticky=tk.N)
 
-	def __init_interface(self):
+	def init_interface(self):
 		self.create_buttons()
 
 
 class StatusFrame(IFrame):
-	def __init__(self, container):
-		super().__init__(container)
-		self.color1 = self.cs.config.get('match', 'color1')
-		self.color2 = self.cs.config.get('match', 'color2')
-		self.__init_interface()
-
-	def create_color(self):
-		self.change_color = tk.Frame(self, relief="raised", background=self.cs.match.get_color(), width=500, height=400)
-		self.change_color.grid(column=5, row=5)
+	def __init__(self, root, parent):
+		super().__init__(root, parent)
+		self.color1 = self.root.cs.config.get('match', 'color1')
+		self.color2 = self.root.cs.config.get('match', 'color2')
+		self.init_interface()
 
 	def recalibration(self):
-		print("Bonjour recalibration")
-		self.cs.ai[ROBOT].reset(True)
+		self.root.cs.ai[ROBOT].reset(True)
 
 	def close(self):
-		self.parent.close()
+		self.root.close()
 
 	def reset_position(self):
-		self.cs.ai[ROBOT].reset(False)
+		self.root.cs.ai[ROBOT].reset(False)
 
 	def reset_match(self):
 		try:
-			self.cs.match.reset_match()
+			self.root.cs.match.reset_match()
 		except Exception as e:
 			print('[IA INTERFACE] Failed to reset match : %s' % str(e))
 
-	def slurp(self):
-		print("Bonjour slurping")
-		self.cs.robot[ROBOT].fill_n_cherries(10)
-
-	def slurp_less(self):
-		print("Bonjour slurping un peu moins")
-		self.cs.robot[ROBOT].fill_n_cherries(2)
-
-	def slurp_set(self):
-		print("Bonjour slurping le set")
-		self.cs.robot[ROBOT].set_cherry_count()
-
-	def create_buttons(self):
+	def init_interface(self):
 		tk.Button(self, text="Recalibrate", command=self.recalibration, font=FONT_MEDIUM).pack(fill=tk.X, side=tk.TOP, pady=4)
 		tk.Button(self, text="Reset position", command=self.reset_position, font=FONT_MEDIUM).pack(fill=tk.X, side=tk.TOP, pady=4)
 		tk.Button(self, text="Reset match", command=self.reset_match, font=FONT_MEDIUM).pack(fill=tk.X, side=tk.TOP, pady=4)
-		tk.Button(self, text="Change color", command=self.action_color, font=FONT_MEDIUM).pack(fill=tk.X, side=tk.TOP, pady=4)
+		tk.Button(self, text="Change color", command=self.change_color, font=FONT_MEDIUM).pack(fill=tk.X, side=tk.TOP, pady=4)
 
-	def __init_interface(self):
-		#self.create_color()
-		self.create_buttons()
-
-	def action_color(self):
-		if self.cs.match.get_color() == self.color1:
-			self.cs.match.set_color(self.color2)
-			self.change_color.config(bg=self.color2)
+	def change_color(self):
+		if self.root.cs.match.get_color() == self.color1:
+			self.root.cs.match.set_color(self.color2)
 		else:
-			self.cs.match.set_color(self.color1)
-			self.change_color.config(bg=self.color1)
+			self.root.cs.match.set_color(self.color1)
+
+
+class HomeInterface(IFrame):
+	def __init__(self, root, parent):
+		super().__init__(root, parent, bg=root.cs.match.get_color())
+		self.init_interface()
+
+	def init_interface(self):
+		self.grid_rowconfigure(0, weight=0)
+		self.grid_rowconfigure(1, weight=1)
+		self.grid_columnconfigure(0, weight=1)
+		self.grid_columnconfigure(1, weight=1)
+
+		self.button_system_frame = ButtonSystem(self.root, self)
+		self.button_system_frame.grid(row=0, column=0, columnspan=2, padx=4, pady=4)
+
+		self.strategies_frame = StrategyFrame(self.root, self)
+		self.strategies_frame.grid(row=1, column=0, padx=8, pady=8)
+
+		self.status_frame = StatusFrame(self.root, self)
+		self.status_frame.grid(row=1, column=1, padx=8, pady=8)
 
 
 class MatchInterface(IFrame):
-	def __init__(self, container):
-		super().__init__(container, bg=container.cs.match.get_color())
-		self.__init_interface()
+	def __init__(self, root, parent):
+		super().__init__(root, parent, bg=root.cs.match.get_color())
+		self.init_interface()
 
 	def close(self):
-		self.parent.close()
+		self.root.cs.match.reset_match()
 
-	def __init_interface(self):
-		self.grid_rowconfigure(0, weight=1, uniform="r")
-		self.grid_rowconfigure(1, weight=1, uniform="r")
-		self.grid_rowconfigure(2, weight=1, uniform="r")
-		self.grid_columnconfigure(0, weight=1)
-		tk.Button(self, text="Close", command=self.close, font=FONT_MEDIUM).grid(column=0, row=0, sticky=tk.NW)
+	def init_interface(self):
+		topbar = tk.Frame(self)
+		topbar.pack(side=tk.TOP, fill=tk.X, expand=True)
+		tk.Button(topbar, text="Close", command=self.close, font=FONT_MEDIUM).pack(side=tk.LEFT)
 		self.text = tk.Label(self, text=f"Score: 0", font=FONT_BIG)
-		self.text.grid(column=0, row=1)
+		self.text.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
 
 	def update_interface(self):
-		match_status = self.cs.match.get_status()
+		match_status = self.root.cs.match.get_status()
 		self.text.config(text=f"Score: {match_status['score']}")
 
 
 class AIInterface(Interface):
 	def __init__(self):
-		self.cs = CellaservProxy()
-		self.color = self.cs.match.get_color()
 		super().__init__('AI')
+		self.cs = CellaservProxy()
 		self.init_fonts()
-		self.window.after(self.interface_refresh, self.update_interface)
-		self.create_widget()
-		self.update_interface()
+		self.match_interface = MatchInterface(self, self.window)
+		self.home_interface = HomeInterface(self, self.window)
+		self.current_frame = None
+
+	def set_frame(self, frame):
+		if frame == self.current_frame:
+			return
+		if self.current_frame is not None:
+			self.current_frame.pack_forget()
+		self.current_frame = frame
+		self.current_frame.pack(expand=True, fill=tk.BOTH)
 
 	def init_fonts(self):
 		global FONT_BIG, FONT_MEDIUM, FONT_SMALL
@@ -186,48 +181,17 @@ class AIInterface(Interface):
 		FONT_MEDIUM = tkinter.font.Font(self.window, size=24)
 		FONT_SMALL = tkinter.font.Font(self.window, size=16)
 
-	def create_widget(self, start_match=False):
-		if not start_match:
-			self.window.grid_rowconfigure(0, weight=0)
-			self.window.grid_rowconfigure(1, weight=1)
-			self.window.grid_columnconfigure(0, weight=1)
-			self.window.grid_columnconfigure(1, weight=1)
-
-			self.button_system_frame = ButtonSystem(self)
-			self.button_system_frame.grid(row=0, column=0, columnspan=2, padx=4, pady=4)
-
-			self.strategies_frame = StrategyFrame(self)
-			self.strategies_frame.grid(row=1, column=0, padx=8, pady=8)
-
-			self.status_frame = StatusFrame(self)
-			self.status_frame.grid(row=1, column=1, padx=8, pady=8)
-		else:
-			self.match_interfaces_frame = MatchInterface(self)
-			self.match_interfaces_frame.pack(expand=True, fill=tk.BOTH)
-
 	def update_interface(self):
 		match_status = self.cs.match.get_status()
 		self.window.configure(bd=5, highlightcolor=self.cs.match.get_color(), highlightthickness=5)
 		if match_status["status"] == "Started" or match_status["status"] == "Ended":
 			print("[+] Match is running")
-			if not self.reset:
-				self.status_frame.destroy()
-				self.button_system_frame.destroy()
-				self.strategies_frame.destroy()
-				self.reset = True
-				self.create_widget(True)
-			else:
-				self.match_interfaces_frame.update_interface()
+			self.set_frame(self.match_interface)
+			self.match_interface.update_interface()
 		else:
-			print("[+] Fix match not running or problem with this")
-			if not self.reset:
-				self.status_frame.update_interface()
-				self.button_system_frame.update_interface()
-				self.strategies_frame.update_interface()
-			else:
-				self.reset = False
-				self.create_widget()
-				self.match_interfaces_frame.destroy()
+			print("[+] Match is not running")
+			self.set_frame(self.home_interface)
+			self.home_interface.update_interface()
 		self.window.after(self.interface_refresh, self.update_interface)
 
 
@@ -235,7 +199,6 @@ def main():
     if len(argv) > 1:
        global ROBOT
     interface = AIInterface()
-    interface.window.configure(bd=0)
     interface.loop()
 
 
