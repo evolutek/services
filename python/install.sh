07c13b50725652fbd758b5b698e544aa222d7415
Kolte200 <celian.aldehuelo@gmail.com>
2025-04-19 02:53:52 +0200

    change services config

--- python/install.sh
+++ python/install.sh
@@ -2,4 +2,12 @@
 
 export PYTHON="which python | head -n1"
 
-cat "" | envsubst > /etc/systemd/system/
+function install {
+    cat "systemd/$1" | envsubst > "/etc/systemd/system/$1"
+}
+
+install "actuators.service"
+install "ai.service"
+install "robot.service"
+install "trajman.service"
+install "reset.service"
